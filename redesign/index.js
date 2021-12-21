a584aa0044fc6f8daefc4e3e81c0dafed2a0dfb6
Zach Yadegari <zay0106@icloud.com>
2021-12-21 15:02:29 -0500

    redesign v2

---
+++ redesign/index.js
@@ -0,0 +1,44 @@
+let numberOfScans = 1
+
+
+//let firstHeading = document.getElementById("first-heading");
+
+//console.log(firstHeading.getBoundingClientRect());
+
+function isVisible(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = -100; 
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function scanDocument() {
+    let sectionList = document.querySelectorAll('.hidden');
+    sectionList.forEach(function(section) {
+        if(isVisible(section)) {
+            section.classList.remove('hidden');
+            section.classList.add('text-fade');
+        };
+    });
+
+    console.log(numberOfScans);
+    numberOfScans++;
+}
+
+
+document.addEventListener("scroll", scanDocument);
+
+
+// function throttle(fn, wait) {
+//     var time = Date.now();
+//     return function() {
+//       if ((time + wait - Date.now()) < 0) {
+//         fn();
+//         time = Date.now();
+//       }
+//     }
+//   }
