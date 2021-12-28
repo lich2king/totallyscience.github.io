97440b2349b0170807956d826a16ab54f2c20d7d
Henry Langmack <Henry.langmack@gmail.com>
2021-12-27 22:25:18 -0500

    more json but iframe no worky :(

--- redesign/games/load_game.js
+++
@@ -1,3 +0,0 @@
-const queryString = location.search
-const urlParams = new URLSearchParams(queryString);
-const gamesJSON = fetch("./games.json").then(response => response.json()).then(data => console.log(data))
