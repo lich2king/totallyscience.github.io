4aa0b903e582064054387c33b9df9e123540e090
Zach Yadegari <zay0106@icloud.com>
2021-12-29 00:00:56 -0500

    gamestotopbuttons

--- redesign/games.js
+++ redesign/games.js
@@ -136,3 +136,21 @@ letters.forEach((letter) => {
         window.scrollTo({top: scroll, behavior: 'smooth'})
     })
 })
+
+
+//Code for scroll button
+//Get the button
+var mybutton = document.getElementById('scrollb')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        mybutton.style.display = 'block'
+    } else {
+        mybutton.style.display = 'none'
+    }
+}
