06c7a61b43f1c493fb045ef9843db353c489b4e9
Henry Langmack <Henry.langmack@gmail.com>
2021-12-29 00:16:57 -0500

    suffixed string

--- redesign/visits_count.js
+++ redesign/visits_count.js
@@ -1,6 +1,17 @@
+const addSuffix = num => {
+    if(num.endsWith('1')) {
+        return num + 'st'
+    } else if(num.endsWith('2')) {
+        return num + 'nd'
+    } else if(num.endsWith('3')) {
+        return num + 'rd'
+    }
+    return num + 'th'
+}
+
 fetch('./stats/counter.php')
     .then((response) => response.text())
     .then((visits) => {
         const display = document.getElementById("visits-count")
-        display.innerText = visits
+        display.innerText = addSuffix(visits)
     })
