1513dcce071df3010ca1a8e455c098f1369e1123
Ilan <ilanluciano1@gmail.com>
2021-12-28 20:36:38 -0500

    Styling
    Fix game controls alignment, stop selecting of letters on games, fix footer image alignment, add panic button to redesign, and put navbar in main css file

---
+++ redesign/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('keydown', function(e) {
+    if (e.key == '`') {
+        window.open('https://classroom.google.com/u/0/h', '_blank', 'fullscreen=yes resizable=yes');
+    }
+}, false);
