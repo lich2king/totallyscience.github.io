c2c565ee3603d7d41f9968af0b8c329f265fd0f7
Henry Langmack <Henry.langmack@gmail.com>
2021-12-31 11:09:06 -0500

    php testing

--- redesign/php/counter.php
+++ redesign/php/counter.php
@@ -1,25 +1,20 @@
 <?php
-$servername = "localhost";
-$database = "u483325885_database";
-$username = "u483325885_user";
-$password = "Totally_password4321";
 
-$conn = new mysqli($servername, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
-}
+$pdo = require "connect.php";
 
-$sql = "select visits from stats where id=1";
-$result = $conn->query($sql);
+echo($pdo);
 
-if($result->num_rows > 0) {
-    $visits_str = $result->fetch_assoc()["visits"];
-    $visits = $visits_str + 1;
-    echo($visits);
+// $sql = "select visits from stats where id=1";
+// $result = $conn->query($sql);
 
-    $update_sql = "update stats set visits={$visits} where id=1";
-    $conn->query($update_sql);
-}
+// if($result->num_rows > 0) {
+//     $visits_str = $result->fetch_assoc()["visits"];
+//     $visits = $visits_str + 1;
+//     echo($visits);
 
-$conn->close();
+//     $update_sql = "update stats set visits={$visits} where id=1";
+//     $conn->query($update_sql);
+// }
+
+// $conn->close();
 ?>
---
+++ redesign/php/report.php
@@ -0,0 +1,7 @@
+<?php
+// $conn = new mysqli($servername, $username, $password, $database);
+// if ($conn->connect_error) {
+//     die("Connection Failed: " . $conn->connect_error);
+// }
+// $conn->close();
+?>
