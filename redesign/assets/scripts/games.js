6084ac3e664f1c49671e182bc1c099b7ea6bc562
HackedZach <zay0106@icloud.com>
2022-01-01 11:02:22 -0500

    when bolts clicked new gets selected

--- redesign/assets/scripts/games.js
+++ redesign/assets/scripts/games.js
@@ -58,11 +58,13 @@ fetch('./assets/games.json')
 //game catagories
 //selected topic variable is used both in the button category changer and the search bar function
 let selectedTopic = 'all'
-const buttons = $qsa('.categoryButton')
+const buttons = $qsa('.categoryButton, #bolt')
+console.log(buttons)
+//buttons.appendChild(document.getElementById('bolt'))
 //add event listener to every element in the document with the class "categoryButton"
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '⚡') {
+        if (e.target.id == 'bolt') {
             selectedTopic = e.target.parentNode.name
         } else {
             selectedTopic = e.target.name
