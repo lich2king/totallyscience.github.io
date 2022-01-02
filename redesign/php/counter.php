089e490522b9b996120cbb04a2e527d83609812b
Henry Langmack <Henry.langmack@gmail.com>
2022-01-01 23:32:01 -0500

    working counter with new sql setup

--- redesign/php/counter.php
+++ redesign/php/counter.php
@@ -1,8 +1,21 @@
 <?php
 $pdo = require "connect.php";
 
-$visits = $pdo->query("select visits from stats where id=1")->fetch()["visits"] + 1;
-echo($visits);
+$datetime = new DateTime("now", new DateTimeZone("America/New_York"));
+$day = $datetime->format("M d Y");
 
-$pdo->prepare("update stats set visits=? where id=1")->execute([$visits]);
+$select = $pdo->prepare("select id, count from stats where day=?");
+$select->execute([$day]);
+$data = $select->fetch();
+
+if(!$data) {
+    $pdo->prepare("insert into stats (day, count) values (?, 1)")->execute([$day]);
+    echo(1);
+    exit;
+} else {
+    $count = $data["count"] + 1;
+    $pdo->prepare("update stats set count=? where id=?")->execute([$count, $data["id"]]);
+    $total = $pdo->query("select sum(count) as total from stats")->fetch()["total"];
+    echo($total);
+}
 ?>
