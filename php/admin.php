91948ab673574462745bf9a89e9c5180df277897
Henry Langmack <Henry.langmack@gmail.com>
2022-01-02 16:20:28 -0500

    rename table state to views

--- php/admin.php
+++ php/admin.php
@@ -30,9 +30,9 @@ while($report = $select->fetch()) {
     echo("<tr><td>" . $report["name"] . "</td><td>" . $report["count"] . "</td></tr>");
 }
 
-echo("</table><h1>Page Views</h1><table><tr><th>Day</th><th>Count</th></tr>");
-$select = $pdo->query("select day, count from stats");
-$total = $pdo->query("select sum(count) as total from stats")->fetch();
+echo("</table><h1>Daily Homepage Visits (does not include games)</h1><table><tr><th>Day</th><th>Count</th></tr>");
+$select = $pdo->query("select day, count from views");
+$total = $pdo->query("select sum(count) as total from views")->fetch();
 $datedata = "";
 while($day = $select->fetch()) {
     $datedata = "<tr><td>" . $day["day"] . "</td><td>" . $day["count"] . "</td></tr>" . $datedata;
