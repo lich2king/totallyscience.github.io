13aac1ba3f286214455caa123dbc332fc09ef425
Henry Langmack <Henry.langmack@gmail.com>
2022-01-03 17:23:30 -0500

    use soundbites.xyz for backend

--- assets/scripts/visits_count.js
+++ assets/scripts/visits_count.js
@@ -9,7 +9,7 @@ const addSuffix = (num) => {
     return num + 'th'
 }
 
-fetch('./php/counter.php')
+fetch('https://soundbites.xyz/totallyscience-backend/counter.php')
     .then((response) => response.text())
     .then((visits) => {
         const display = $('visits-count')
