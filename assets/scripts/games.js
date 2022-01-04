6760c98597bf8ab97a665f9c9403f48e8c2fe332
HackedZach <zay0106@icloud.com>
2022-01-04 17:58:29 -0500

    newadss

--- assets/scripts/games.js
+++ assets/scripts/games.js
@@ -65,7 +65,7 @@ fetch(`./assets/games.json?${new Date().getTime()}`)
                 
                 const adIns = document.createElement("ins")
                 adIns.setAttribute("class", "adsbygoogle")
-                adIns.setAttribute("style", "display:inline-block;width:550px;height:110px")
+                adIns.setAttribute("style", "display:inline-block;width:525px;height:105px")
                 //adIns.setAttribute("data-ad-format", "auto")
                 //adIns.setAttribute("data-ad-layout-key", "-gs+3s+5e-7t-2r")
                 adIns.setAttribute("data-ad-client", "ca-pub-3486863589051210")
@@ -75,7 +75,7 @@ fetch(`./assets/games.json?${new Date().getTime()}`)
                 adScriptEnd.innerText="(adsbygoogle = window.adsbygoogle || []).push({});"
 
                 const adDivCushion = document.createElement("div")
-                adDivCushion.style = "padding-bottom: 2%"
+                adDivCushion.style = "padding-bottom: 10%"
                 
                 existingSection.insertAdjacentElement('afterend', adScript);
                 existingSection.insertAdjacentElement('afterend', adIns);
