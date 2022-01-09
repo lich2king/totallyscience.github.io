a9d3473381eddaaf1fefe4ea69f96571b8aaa3fe
HackedZach <zay0106@icloud.com>
2022-01-09 15:18:41 -0500

    disguise feature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,7 @@ const mkHtml = (code) => { // Unsafe, only use with trusted input.
 
 window.addEventListener(
     'keydown',
-    function (e) {
+    function(e) {
         if (e.key == '`') {
             window.open(
                 this.localStorage.getItem("website"),
@@ -59,6 +59,23 @@ if (localStorage.getItem("website") == null) {
 
 if (localStorage.getItem("theme") == null) {
     localStorage.setItem("theme", "light")
+}
+
+if (localStorage.getItem("disguise") == null) {
+    localStorage.setItem("disguise", "none")
+} else {
+    const favicon = document.querySelector('[rel=icon]')
+    const title = document.querySelector('title')
+    if (localStorage.getItem("disguise") == "gc") {
+        title.innerHTML = "Google Classroom"
+        favicon.href = "./assets/images/disguises/gcicon.png"
+    } else if (localStorage.getItem("disguise") == "gd") {
+        title.innerHTML = "Google Docs"
+        favicon.href = "./assets/images/disguises/gdicon.png"
+    } else if (localStorage.getItem("disguise") == "canvas") {
+        title.innerHTML = "Canvas"
+        favicon.href = "./assets/images/disguises/canvasicon.jpg"
+    }
 }
 
 document.body.setAttribute("theme", localStorage.getItem("theme"))
--- assets/scripts/preferences.js
+++ assets/scripts/preferences.js
@@ -1,18 +1,22 @@
 const websiteInput = $('website')
 const themeInput = $('theme')
 const saveBtn = $('save-button')
+const disguiseInput = $('disguise')
 
 websiteInput.value = localStorage.getItem('website')
 themeInput.value = localStorage.getItem('theme')
+disguiseInput.value = localStorage.getItem('disguise')
 
 $('survey').addEventListener('submit', (e) => {
     e.preventDefault();
 
     let websiteEntered = websiteInput.value
-    const themeEntered = themeInput.value
+    let themeEntered = themeInput.value
+    let disguiseEntered = disguiseInput.value
 
     localStorage.setItem("website", websiteEntered)
     localStorage.setItem("theme", themeEntered)
+    localStorage.setItem("disguise", disguiseEntered)
 
     saveBtn.innerHTML = "Saved."
     setTimeout(() => {
