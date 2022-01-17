ed0a9fbb9bfaf73f03589e2ced11eab664016678
HackedZach <zay0106@icloud.com>
2022-01-17 14:40:34 -0500

    settings mobile

---
+++ mobile/assets/scripts/settings.js
@@ -0,0 +1,21 @@
+const websiteInput = $('website')
+const themeInput = $('theme')
+const saveBtn = $('save-button')
+
+websiteInput.value = localStorage.getItem('website')
+themeInput.value = localStorage.getItem('theme')
+
+$('survey').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let websiteEntered = websiteInput.value
+    let themeEntered = themeInput.value
+
+    localStorage.setItem("website", websiteEntered)
+    localStorage.setItem("theme", themeEntered)
+
+    saveBtn.innerHTML = "Saved."
+    setTimeout(() => {
+        saveBtn.innerHTML = "Save"
+    }, 500)
+})
