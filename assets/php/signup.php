f1c1b1afe75b71cc505a34786b38988e7d0fd37f
Zach Yadegari <zay0106@icloud.com>
2022-01-19 14:24:26 -0500

    signuo

---
+++ assets/php/signup.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "u483325885_profile";
+$password = "Totally_password4321";
+$database = "u483325885_accounts";
+
+$username = htmlspecialchars($_GET["username"]);
+$email = htmlspecialchars($_GET["email"]);
+$password = htmlspecialchars($_GET["password"]);
+
+
+die("connect");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+  
+// Check connection
+if ($conn->connect_error) {
+  die("connection failed"); //. $conn->connect_error);
+}
+
+$sql = "INSERT INTO AccountsTable (Username, Email, Password)
+VALUES ('$username', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+?>
