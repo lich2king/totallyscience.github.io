c9b95ea265d844e1d66c0b4325db7a4a5e381bf6
Zach Yadegari <zay0106@icloud.com>
2022-01-19 15:27:22 -0500

    huh

--- assets/php/signup.php
+++ assets/php/signup.php
@@ -7,6 +7,8 @@ $database = "u483325885_accounts";
 $user = htmlspecialchars($_GET["username"]);
 $email = htmlspecialchars($_GET["email"]);
 $pass = htmlspecialchars($_GET["password"]);
+$uservalid = htmlspecialchars($_GET["uservalid"]);
+$emailvalid = htmlspecialchars($_GET["emailvalid"]);
 
 $pass = password_hash($pass, PASSWORD_DEFAULT);
 
@@ -18,17 +20,34 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
+if($uservalid == 0)
+{
+    $userresult = $mysqli->query("SELECT id FROM AccountsTable WHERE Username = '$user'");
+    if($userresult->num_rows == 0) {
+        // row not found, do stuff...
+        die("user not found");
+    } else {
+        // do other stuff...
+        die("user found");
+    }
+} elseif($emailvalid == 0)
+{
 
-$sql = "INSERT INTO AccountsTable (Username, Email, Password)
-VALUES ('$user', '$email', '$pass')";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+else
+{
+    $sql = "INSERT INTO AccountsTable (Username, Email, Password)
+    VALUES ('$user', '$email', '$pass')";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
 }
 
-$conn->close();
 
 
 
