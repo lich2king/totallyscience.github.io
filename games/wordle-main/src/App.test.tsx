7a81a8faa901bb2871bf4210bac75baa72b7026d
Zach Yadegari <zay0106@icloud.com>
2022-01-27 09:59:45 -0500

    n

--- games/wordle-main/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders App component', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Not Wordle/)
-  expect(linkElement).toBeInTheDocument()
-})
