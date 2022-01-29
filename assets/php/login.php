e1a39f1769f2163c78cb23b5617770ee43105c47
HackedZach <zay0106@icloud.com>
2022-01-29 10:27:07 -0500

    papajo

---
+++ assets/php/login.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "u483325885_profile";
+$password = "Totally_password4321";
+$database = "u483325885_accounts";
+
+$user = htmlspecialchars($_GET["username"]);
+$pass = htmlspecialchars($_GET["password"]);
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+  
+// Check connection
+if ($conn->connect_error) {
+  die("connection failed"); //. $conn->connect_error);
+}
+
+
+$result = mysql_query("SELECT *,email FROM people WHERE * = '$user'");
+if (!$result) {
+    echo 'Could not run query: ' . mysql_error();
+    exit;
+}
+$row = mysql_fetch_row($result);
+
+die($row[0]); // 42
+echo $row[1]; // the email value
+
+
+
+
+?>
