317ac84dd7454e6060e370a650a812173da3f6ce
HackedZach <zay0106@icloud.com>
2022-01-29 13:27:27 -0500

    php suck my

--- assets/php/login.php
+++ assets/php/login.php
@@ -1,7 +1,34 @@
 <?php
 
-die("Hello Henrico")
+die("Hello Henrico");
+$config = include "config.php";
 
+$dsn = "mysql:dbname=" . $config["account_database"] . ";host=" . $config["host"];
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $config["account_username"], $config["password"], $options);
+
+
+$user = htmlspecialchars($_GET["username"]);
+$pass = htmlspecialchars($_GET["password"]);
+
+
+
+$query = $pdo->prepare("select * from AccountsTable where Username=?");
+$select->execute(["Zach"]);
+$data = $select->fetch();
+
+if(!$data) {
+    die("Incorrect username or password");
+} else {
+    // Do stuff with $data["Username"] and $data["Password"]
+    die("success")
+}
 
 
 /*$result = $conn->query("SELECT * FROM AccountsTable WHERE Username = 'Zach'");
