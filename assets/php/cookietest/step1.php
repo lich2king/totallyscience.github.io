9e0f1b1d6f4587329b3242b6d13223c17fef9e35
HackedZach <zay0106@icloud.com>
2022-02-21 11:05:04 -0500

    bye

--- assets/php/cookietest/step1.php
+++ assets/php/cookietest/step1.php
@@ -1,6 +1,5 @@
 <?php
   header('Access-Control-Allow-Origin: *');
-  header('Content-Type: application/json');
   // Set test cookie
   setcookie('third_party_c_t', 'hey there!', time() + 3600*24*2);
 ?>
