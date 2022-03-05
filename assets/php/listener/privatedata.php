58edaef794dce8218dd8a2fb777302c23781887f
HackedZach <zay0106@icloud.com>
2022-03-05 15:33:01 -0500

    okay

--- assets/php/listener/privatedata.php
+++ assets/php/listener/privatedata.php
@@ -5,29 +5,35 @@
     $database = "u483325885_private";
 
 
-    $email = htmlspecialchars($_GET["email"]);
     $payerId = htmlspecialchars($_GET["payerId"]);
 
-
-    // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
       
     // Check connection
     if ($conn->connect_error) {
-      die("connection failed"); //. $conn->connect_error);
+        die("connection failed"); //. $conn->connect_error);
     }
 
+    if ($userresult = $conn->query("SELECT * FROM PersonalSites WHERE PayerId = '$payerId'"))
+    {
+        $row = $userresult -> fetch_row();
+        $usersEndDate = $row[5];
+        die($usersEndDate)
+        date_default_timezone_set("America/New_York");
+        $d=strtotime("+1 Months");
+        $endDate = date("Y-m-d", $d);   
+        if($usersEndDate < $endDate){
+            $sql = "UPDATE PersonalSites SET EndDate='$endDate || $usersEndDate' WHERE PayerId='$payerId'";
 
-    $sql = "INSERT INTO PersonalSites (Url, Taken, AccountEmail, Password, PayerId, EndDate)
-    VALUES ('$url', '1', '$email', '$pass', '$payerId', '$endDate')";
+            if ($conn->query($sql) === TRUE) {
+                echo "Success";
+                //echo "New record created successfully";
+            } else {
+                //echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
     
-    if ($conn->query($sql) === TRUE) {
-        echo "Success";
-      //echo "New record created successfully";
-    } else {
-      //echo "Error: " . $sql . "<br>" . $conn->error;
-      //echo "Error";
-    }
     
     $conn->close();
 
