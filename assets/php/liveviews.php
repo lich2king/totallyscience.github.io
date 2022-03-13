267052912fce56ef26e4e118884878ffea7a07cd
HackedZach <zay0106@icloud.com>
2022-03-13 16:42:24 -0400

    wfe

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -12,8 +12,10 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
+date_default_timezone_set("America/New_York");
 $d=strtotime("+1 Minutes");
-$currentTime = date("d-m-Y h:i:s", $d);
-die($currentTime)
+$currentTime = date("d-m-Y h:i:s");
+$otherTime = date("d-m-Y h:i:s", $d);
+die($currentTime, $otherTime)
 
 ?>
