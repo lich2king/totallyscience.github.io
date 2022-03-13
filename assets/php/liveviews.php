3d37ddd808386f940a5e16bd2a01ee54897a435a
HackedZach <zay0106@icloud.com>
2022-03-13 19:10:09 -0400

    ef

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -21,6 +21,7 @@ date_default_timezone_set("America/New_York");
 $query = "SELECT * FROM liveviews WHERE username = '$user'";
 
 if ($conn->query($query) != NULL) {
+    die("BAH");
     $d=strtotime("+1 Minutes");
     $pingTime = date("d-m-Y h:i:s", $d);
 
@@ -34,6 +35,7 @@ if ($conn->query($query) != NULL) {
     }
 
 } else {
+    die("YAHHH");
     $d=strtotime("+1 Minutes");
     $pingTime = date("d-m-Y h:i:s", $d);
     $sql = "INSERT INTO liveviews (Username, lastping)
