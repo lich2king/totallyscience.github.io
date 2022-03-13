e0173cc079c00e2517f1bd7b4b5738759cc36913
HackedZach <zay0106@icloud.com>
2022-03-13 16:57:35 -0400

    ewf

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -18,7 +18,16 @@ if ($conn->connect_error) {
 date_default_timezone_set("America/New_York");
 
 
+if ($userresult = $conn->query("SELECT * FROM liveviews WHERE username = '$user'"))
+{
+    die($userresult);
 
+}
+else
+{
+    //add to db
+    echo("No RESULTS OF USERNAME!");
+}
 
 //After do the for loop to delete old ones
 
