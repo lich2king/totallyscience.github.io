9ffba972740ead8307e7b593d7873976d9ac1dd4
Zach Yadegari <zay0106@icloud.com>
2022-03-14 13:58:28 -0400

    ewf

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -62,7 +62,7 @@ $json = mysqli_fetch_array ($result, MYSQLI_ASSOC);
 
 foreach($json as $value => $item){
   echo($json);
-  echo($json[$value]);
+  echo($json[$value][1]);
   echo($value);
   echo($item[0]);
   echo $item;
