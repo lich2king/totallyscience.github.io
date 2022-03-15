e4a30972e3101e18e5071853ada72a3a068a6ff0
HackedZach <zay0106@icloud.com>
2022-03-15 18:09:29 -0400

    wef

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -56,7 +56,7 @@ if($userresult->num_rows == 0) {
 }
 
 
-$query = "SELECT username FROM liveviews WHERE 2";
+$query = "SELECT username FROM liveviews";
 $result = mysqli_query($conn, $query);
 $json = mysqli_fetch_array ($result, MYSQLI_ASSOC);
 
