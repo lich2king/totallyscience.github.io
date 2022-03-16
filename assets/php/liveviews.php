f9ffdad085ecef416d0a398e17711ceff4e0e446
HackedZach <zay0106@icloud.com>
2022-03-16 19:56:47 -0400

    wef

--- assets/php/liveviews.php
+++ assets/php/liveviews.php
@@ -21,6 +21,7 @@ date_default_timezone_set("America/New_York");
 
 if($leave == '0')
 {
+  die("AQUI");
     $userresult = $conn->query("SELECT * FROM liveviews WHERE username = '$uid'");
     //die($userresult);
     if($userresult->num_rows == 0) {
@@ -52,6 +53,7 @@ if($leave == '0')
         }
     }
 } else {
+  die("ALLA");
     $sql = "DELETE FROM liveviews WHERE username='$uid'";
 
     if ($conn->query($sql) === TRUE) {
