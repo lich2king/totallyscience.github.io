7d939c868840b66dbcf610d168e258b8cf234a93
HackedZach <zay0106@icloud.com>
2022-03-16 19:54:36 -0400

    r3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -128,7 +128,7 @@ function userExitLiveViews() {
 
 
 window.addEventListener("beforeunload", function(event) {
-    userExitLiveViews();
+    //userExitLiveViews();
     e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
     // Chrome requires returnValue to be set
     e.returnValue = '';
