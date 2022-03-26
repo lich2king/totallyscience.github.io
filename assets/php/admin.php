8f5cd1928bed87a9610a2746cebe1ee3177f3f25
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-03-25 20:14:27 -0400

    Update admin.php

--- assets/php/admin.php
+++ assets/php/admin.php
@@ -33,11 +33,8 @@ date_default_timezone_set("America/New_York");
 while ($live = $select->fetch()) {
     $currentDateMinutes = date("d") * 24 * 60 + date("h") * 60 + date("i");
     $pingedDateMinutes = date("d", $live['lastping']) * 24 * 60 + date("h", $live['lastping']) * 60 + date("i", $live['lastping']);
-
-    if ($liveViewerCount == 29640) {
-      echo $currentDateMinutes - $pingedDateMinutes;
-    }
-    if (date_diff(date("d-m-Y h:i:s"), $live['lastping']) > 1800000) {
+    
+    if ($currentDateMinutes - $pingedDateMinutes > 30) {
       $uid = $live['username'];
 
       //$pdo->query("delete from liveviews where username='$uid'");
