42906a6cd5c3495b918bddcc42636e7be5c0170d
Zach Yadegari <zay0106@icloud.com>
2022-03-28 11:21:34 -0400

    submit

--- assets/php/submithighscore.php
+++ assets/php/submithighscore.php
@@ -8,7 +8,13 @@ $database = "u483325885_database";
 $username = htmlspecialchars($_GET["username"]);
 $game = htmlspecialchars($_GET["game"]);
 $score = htmlspecialchars($_GET["score"]);
-$imageBlob = htmlspecialchars($_GET["imageBlob"]);
+$imageFile = htmlspecialchars($_GET["imageFile"]);
+
+$baseImage = file_get_contents($imageFile);
+$baseImage = base64_encode($baseImage);
+
+die($baseImage);
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -18,6 +24,18 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
+
+/*
+$userresult = $conn->query("SELECT * FROM AccountsTable WHERE Email = '$email'");
+    //die($userresult);
+    if($userresult->num_rows == 0) {
+        // row not found, do stuff...
+        echo("not found");
+    } else {
+        // do other stuff...
+        echo("found");
+    }
+    */
 
 
    
