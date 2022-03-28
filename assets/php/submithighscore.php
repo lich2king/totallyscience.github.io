3e80799f3df72eff534f20a99fc8f65ef4f96eba
Zach Yadegari <zay0106@icloud.com>
2022-03-28 13:19:31 -0400

    wef

--- assets/php/submithighscore.php
+++ assets/php/submithighscore.php
@@ -13,7 +13,7 @@ $imageFile = htmlspecialchars($_GET["imageFile"]);
 $baseImage = file_get_contents($imageFile);
 $baseImage = base64_encode($baseImage);
 
-die("hi");
+die($baseImage);
 
 
 // Create connection
