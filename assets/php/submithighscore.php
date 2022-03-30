e9bbf6a069e14246932700fca2a6485245184949
Zach Yadegari <zay0106@icloud.com>
2022-03-30 14:06:44 -0400

    mr vogt

--- assets/php/submithighscore.php
+++ assets/php/submithighscore.php
@@ -5,7 +5,7 @@ $password = "Totally_password4321";
 $database = "u483325885_database";
 
 
-$username = htmlspecialchars($_GET["username"]);
+$user = htmlspecialchars($_GET["username"]);
 $game = htmlspecialchars($_GET["game"]);
 $score = htmlspecialchars($_GET["score"]);
 $imageFile = htmlspecialchars($_GET["imageFile"]);
@@ -13,10 +13,7 @@ $imageFile = htmlspecialchars($_GET["imageFile"]);
 //echo($imageFile);
 //$baseImage = file_get_contents($imageFile);
 //echo($baseImage);
-$baseImage = base64_encode($imageFile);
 
-
-die($baseImage);
 
 
 // Create connection
@@ -28,17 +25,20 @@ if ($conn->connect_error) {
 }
 
 
-/*
-$userresult = $conn->query("SELECT * FROM AccountsTable WHERE Email = '$email'");
-    //die($userresult);
-    if($userresult->num_rows == 0) {
-        // row not found, do stuff...
-        echo("not found");
-    } else {
-        // do other stuff...
-        echo("found");
-    }
-    */
+$sql = "INSERT INTO highscore_requests (game, score, username, image)
+VALUES ('$game', '$score', '$user', '$imageFile')";
+    
+if ($conn->query($sql) === TRUE) {
+    echo "Success";
+    //echo "New record created successfully";
+} else {
+    //echo "Error: " . $sql . "<br>" . $conn->error;
+    //echo "Error";
+}
+    
+$conn->close();
+
+    
 
 
    
