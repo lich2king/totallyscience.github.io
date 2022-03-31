298fb980ca758e42e65397b67e19177651b26ecf
Zach Yadegari <zay0106@icloud.com>
2022-03-31 12:15:59 -0400

    h

--- assets/php/submithighscore.php
+++ assets/php/submithighscore.php
@@ -51,9 +51,8 @@ if($highscoreresult->num_rows == 0) {
 function uploadHighscore()
 {
   global $game, $score, $user, $imageFile;
-  
-  $sql = "INSERT INTO highscore_requests (game, score, username, image)
-  VALUES ('$game', '$score', '$user', '$imageFile')";
+
+  $sql = "INSERT INTO highscore_requests (game, score, username, image) VALUES ('$game', '$score', '$user', '$imageFile')";
       
   if ($conn->query($sql) === TRUE) {
       echo ("Success, your score will be reviewed shortly");
