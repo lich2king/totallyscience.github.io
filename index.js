d1d2c35e50c16a4038b8ca36323fbae4c723720b
HackedZach <zay0106@icloud.com>
2022-04-30 19:23:34 -0400

    Css fixes, removed proxy apps, removed star feature from games

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/classes', (req, res) => {
-    res.render('classes', { classes: 'selected'});
+    res.render('classes', { classes: 'selected' });
 });
 app.get('/class', (req, res) => {
     if (req.query.class == '') res.redirect('/classes');
-    
+
     let star = '<path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>';
 
     if (req.cookies['accessToken']) {
@@ -43,7 +43,7 @@ app.get('/class', (req, res) => {
                 if (users[decoded.user].hasOwnProperty('starred_games') && users[decoded.user].starred_games[req.query.class] === true) {
                     star = '<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>';
                 }
-    
+
                 res.render('class', { classes: 'selected', star: star });
             });
         } catch (error) {
@@ -105,14 +105,17 @@ app.post('/highscore', bodyParser.json({ limit: '1.1mb' }), (req, res) => {
 });
 
 app.get('/leaderboard', (req, res) => {
-    res.render('leaderboard', { leaderboard: 'selected'});
+    res.render('leaderboard', { leaderboard: 'selected' });
 });
 
 app.get('/applications', (req, res) => {
-    res.render('apps', { apps: 'selected'});
+    res.render('apps', { apps: 'selected' });
 });
 app.get('/app', (req, res) => {
-    res.render('app', { apps: 'selected'});
+    res.render('app', { apps: 'selected' });
+});
+app.get('/submithighscore', (req, res) => {
+    res.render('submithighscore');
 });
 
 app.get('/profile', (req, res) => {
@@ -120,7 +123,7 @@ app.get('/profile', (req, res) => {
         try {
             jwt.verify(req.cookies['accessToken'], process.env.TOKEN_SECRET, (err, decoded) => {
                 if (err) throw err;
-    
+
                 res.render('profile', { username: decoded.user, starred_games: getStarredGames(decoded.user) });
             });
         } catch (error) {
@@ -176,7 +179,7 @@ io.on('connection', (socket) => {
     } else {
         stats.page_views[page] += 1;
     }
- 
+
     socket.on('disconnect', () => {
         liveUsers -= 1;
     });
@@ -185,7 +188,7 @@ io.on('connection', (socket) => {
 // save live users every hour
 function updateLiveViews() {
     const d = new Date();
-    
+
     stats.live_views[`${d.getMonth()}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:00`] = liveUsers;
 
     setTimeout(updateLiveViews, 1000 * 60 * 60);
@@ -220,7 +223,7 @@ const addSuffix = (num) => {
 // ---------------------------------------------------
 app.post('/signup', bodyParser.json(), (req, res) => {
     const { username, password, confirmPassword, email } = req.body;
-    
+
     if (!username || !password || !confirmPassword || !email) return res.send('all fields are required');
     if (confirmPassword != password) return res.send('passwords must be the same');
 
@@ -245,7 +248,7 @@ app.post('/signup', bodyParser.json(), (req, res) => {
 
 app.post('/login', bodyParser.json(), (req, res) => {
     const { username, password } = req.body;
-    
+
     if (!username || !password) return res.send('all fields are required');
 
 
@@ -287,7 +290,7 @@ function getStarredGames(user) {
             starredGames += `<a href="/class?class=${game}">${game}</a><br>`;
         }
     }
-    
+
     return starredGames;
 }
 
@@ -348,7 +351,7 @@ function saveAndStop() {
 
             fs.writeFile('highscores.json', JSON.stringify(highscores), (err) => {
                 if (err) console.log(err);
-    
+
                 process.exit();
             });
         });
