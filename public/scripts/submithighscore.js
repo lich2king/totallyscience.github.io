240c75c82d5132d89b38211405bada0f5305c152
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-05-21 15:33:22 -0400

    finish fixing errors with highscore page
    form is done just need backend for submissions and approvals

---
+++ public/scripts/submithighscore.js
@@ -0,0 +1,41 @@
+let select = document.getElementById('gamesSelect');
+
+fetch(`/gamesjson`).then((response) => response.json()).then((retrievedGames) => {
+    for (game in retrievedGames) {
+        select.innerHTML += `<option value="${game}">${game}</option>`;
+    }
+});
+
+
+function handleFiles() {
+    let upload = document.getElementById('fileUpload');
+
+    if (upload.files && upload.files[0]) {
+        const file = upload.files[0]
+        const fileType = file['type'];
+        const validImageTypes = ['image/gif', 'image/jpeg', 'image/png', 'image/jpg'];
+
+        if (!validImageTypes.includes(fileType)) {
+            alert('invalid image');
+            document.querySelector('input[type="file"]').value = null;
+            imageFile = '';
+        } else if (file.size > 1000000) {
+            alert('image must be under 1mb');
+            document.querySelector('input[type="file"]').value = null;
+            imageFile = '';
+        } else {
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function() {
+                imageFile = reader.result
+            };
+            reader.onerror = function(error) {
+                console.log('Error: ', error);
+            };
+        }
+    }
+}
+
+function SubmitHighscore() {
+    
+}
