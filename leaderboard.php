959fb0bb538e5b31155d190866c71d639b7c94ad
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-06-16 17:22:53 -0400

    switch files to php and add head include

--- leaderboard.php
+++ leaderboard.php
@@ -2,17 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta property="og:title" content="Totally Science">
-    <meta property="og:site_name" content="Totally Science">
-    <meta property="og:url" content="">
-    <meta property="og:description" content="The best place for 'Learning Science!'">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="https://totallyscience.co/assets/images/logo.png">
-
-    <title>Totally Science</title>
-
-    <link rel="stylesheet" href="assets/styles/main.css">
-    <link rel="icon" href="assets/images/logo.png">
+    <?php include "assets/includes/head.php" ?>
 
     <style>
         button {
@@ -76,22 +66,6 @@
             color: white;
         }
     </style>
-
-    <!--Google Adsense-->
-    <script data-ad-client="ca-pub-3486863589051210" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y14RZLW8H8"></script>
-
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-Y14RZLW8H8');
-    </script>
 </head>
 
 <body>
--- uploadhighscore.php
+++ uploadhighscore.php
@@ -2,42 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta property="og:title" content="Totally Science">
-    <meta property="og:site_name" content="Totally Science">
-    <meta property="og:url" content="">
-    <meta property="og:description" content="The best place for 'Learning Science!'">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="https://totallyscience.co/assets/images/logo.png">
-
-    <title>Totally Science</title>
-    <meta charset="UTF-8">
-
-    <!--Cache Killer-->
-    <script>
-        const mainCSS = `<link rel='stylesheet' href='assets/styles/main.css?${new Date().getTime()}'>`
-        document.getElementsByTagName('head')[0].innerHTML += mainCSS
-    </script>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="0" />
-    <!--End Cache Killer-->
-
-    <link rel="icon" href="assets/images/logo.png">
-
-    <!--Google Adsense-->
-    <script data-ad-client="ca-pub-3486863589051210" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y14RZLW8H8"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-Y14RZLW8H8');
-    </script>
+    <?php include "assets/includes/head.php" ?>
 
     <style>
         #survey {
