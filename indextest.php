9301549e7365ef770a5fefca3bc5974ab885db4b
HackedZach <zay0106@icloud.com>
2022-06-17 14:21:17 -0400

    Index started rewrite of navbar and content

---
+++ indextest.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/indextest.css'>
+
+    <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+    <div id="startcontent">
+        <div id="feature">
+            <img src="https://www.pockettactics.com/wp-content/uploads/2020/03/Retro-bowl-hero.jpg">
+            <div id="arrows">
+                <img src="https://www.cooperlighting.com/s/vividtune/png/left-arrow.png">
+                <img src="https://www.cooperlighting.com/s/vividtune/png/right-arrow.png">
+            </div>
+            <div id="text">
+                <p class="stroke-text smooth-16">RETRO BOWL</p>
+            </div>
+        </div>
+
+        <div id="scisuggests">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+        </div>
+    </div>
+    
+    <div id="info">
+        <p><span style="font-size: 2.25vw;">&#129323;</span> To quickly return to Google Classroom, press <img style="width:3vw;" src="https://img.icons8.com/color/480/tilde-key.png"></p>
+        <p>Click <span style="text-decoration: underline">here</span> to view alternate links</p>
+    <div>
+
+
+    <script src="assets/scripts/main.js"></script>
+    <script src="assets/scripts/index.js"></script>
+</body>
+
+</html>
