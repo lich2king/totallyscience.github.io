ffa170e2bdabfe0151837f0a1ca3b702cfa0fc26
HackedZach <zay0106@icloud.com>
2022-06-17 23:58:45 -0400

    work on index

--- indextest.php
+++ indextest.php
@@ -25,19 +25,45 @@
         </div>
 
         <div id="scisuggests">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
-            <img src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
+            <img
+                src="https://store-images.s-microsoft.com/image/apps.10897.13510798883898003.c2c8bc14-3a39-4806-b549-e42939afea6a.53beb45d-d382-43db-962f-19ccb4b868c2">
         </div>
     </div>
-    
+
     <div id="info">
-        <p><span style="font-size: 2.25vw;">&#129323;</span> To quickly return to Google Classroom, press <img style="width:3vw;" src="https://img.icons8.com/color/480/tilde-key.png"></p>
-        <p>Click <span style="text-decoration: underline">here</span> to view alternate links</p>
-    <div>
+        <p><span style="font-size: 2.25vw;">&#129323;</span> To quickly return to Google Classroom, press <img
+                style="width:3vw;" src="https://img.icons8.com/color/480/tilde-key.png"></p>
+        <p>Click <span style="text-decoration: underline"><b>here</b></span> to view alternate links</p>
+    </div>
+
+    <div id="searchcat">
+        <button name="all" onclick="" class="selectedCategory categoryButton">All</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Some</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Couple</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Few</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Fun</button>
+        <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+        <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+    </div>
 
 
     <script src="assets/scripts/main.js"></script>
