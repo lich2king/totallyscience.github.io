563b5324bdd8d0ed8d37d10669f272b015ef1658
HackedZach <zay0106@icloud.com>
2022-06-23 01:14:21 -0400

    search bar, game categories, and game grid

--- indextest.php
+++ indextest.php
@@ -47,22 +47,64 @@
     </div>
 
     <div id="searchcat">
-        <button name="all" onclick="" class="selectedCategory categoryButton">All</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Some</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Couple</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Few</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Fun</button>
-        <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
-        <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+        <div id="firstcatrow">
+            <button name="all" onclick="" class="selectedCategory categoryButton">All</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Some</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Couple</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Few</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Fun</button>
+            <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+        </div>
+        <div id="nextcatrow">
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+        </div>
+    </div>
+
+    <div id="games">
+        <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+        <input type="image" src="https://images.crazygames.com/games/1v1-lol/cover-1585728351086.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop" />
+        <input type="image" src="https://www.unixmen.com/wp-content/uploads/2014/04/20482.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/cgrgOLuBaZ8/maxresdefault.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/Ae7ekpmZDoQ/hqdefault.jpg" />
+        <input type="image" src="https://cdn.cloudflare.steamstatic.com/steam/apps/319510/header.jpg?t=1579635996" />
+        <input type="image" src="https://play-lh.googleusercontent.com/2QmDKq9ltNj7mZTtWu-sagHMMrL4LrtyHrx3Wzcs-xRBp4gyZG2kMTE4iEWmv5cz5MQ" />
+        <input type="image" src="https://heroconcept.com/wp-content/uploads/2018/09/arkanoid-tribute.jpg" />
+        <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+        <input type="image" src="https://images.crazygames.com/games/1v1-lol/cover-1585728351086.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop" />
+        <input type="image" src="https://www.unixmen.com/wp-content/uploads/2014/04/20482.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/cgrgOLuBaZ8/maxresdefault.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/Ae7ekpmZDoQ/hqdefault.jpg" />
+        <input type="image" src="https://cdn.cloudflare.steamstatic.com/steam/apps/319510/header.jpg?t=1579635996" />
+        <input type="image" src="https://play-lh.googleusercontent.com/2QmDKq9ltNj7mZTtWu-sagHMMrL4LrtyHrx3Wzcs-xRBp4gyZG2kMTE4iEWmv5cz5MQ" />
+        <input type="image" src="https://heroconcept.com/wp-content/uploads/2018/09/arkanoid-tribute.jpg" />
+        <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+        <input type="image" src="https://images.crazygames.com/games/1v1-lol/cover-1585728351086.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop" />
+        <input type="image" src="https://www.unixmen.com/wp-content/uploads/2014/04/20482.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/cgrgOLuBaZ8/maxresdefault.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/Ae7ekpmZDoQ/hqdefault.jpg" />
+        <input type="image" src="https://cdn.cloudflare.steamstatic.com/steam/apps/319510/header.jpg?t=1579635996" />
+        <input type="image" src="https://play-lh.googleusercontent.com/2QmDKq9ltNj7mZTtWu-sagHMMrL4LrtyHrx3Wzcs-xRBp4gyZG2kMTE4iEWmv5cz5MQ" />
+        <input type="image" src="https://heroconcept.com/wp-content/uploads/2018/09/arkanoid-tribute.jpg" />
+        <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+        <input type="image" src="https://images.crazygames.com/games/1v1-lol/cover-1585728351086.png?auto=format,compress&q=75&cs=strip&ch=DPR&w=1200&h=630&fit=crop" />
+        <input type="image" src="https://www.unixmen.com/wp-content/uploads/2014/04/20482.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/cgrgOLuBaZ8/maxresdefault.jpg" />
+        <input type="image" src="https://i.ytimg.com/vi/Ae7ekpmZDoQ/hqdefault.jpg" />
+        <input type="image" src="https://cdn.cloudflare.steamstatic.com/steam/apps/319510/header.jpg?t=1579635996" />
+        <input type="image" src="https://play-lh.googleusercontent.com/2QmDKq9ltNj7mZTtWu-sagHMMrL4LrtyHrx3Wzcs-xRBp4gyZG2kMTE4iEWmv5cz5MQ" />
+        <input type="image" src="https://heroconcept.com/wp-content/uploads/2018/09/arkanoid-tribute.jpg" />
     </div>
 
 
