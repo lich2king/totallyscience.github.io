3612a48e450facea41798fe8a7081cc1ba15e295
HackedZach <zay0106@icloud.com>
2022-06-27 19:42:24 -0400

    footer and icons for game pass ad

---
+++ assets/includes/footer.php
@@ -0,0 +1,79 @@
+<div id="gamePassAd">
+        <div>
+            <img src="/assets/images/icons/lock.png" />
+            <p>
+                More Games
+            </p>
+        </div>
+        <div>
+            <img
+                src="/assets/images/icons/save.png" />
+            <p>
+                Save Games
+            </p>
+        </div>
+        <div>
+            <h1>GAME <span>PASS</span></h1>
+            <button onclick="">Get It Now</button>
+        </div>
+        <div>
+            <img
+                src="/assets/images/icons/block-ads.png" />
+            <p>
+                No Ads
+            </p>
+        </div>
+        <div>
+            <img
+                src="/assets/images/icons/eye.png" />
+            <p>
+                More Links
+            </p>
+        </div>
+    </div>
+    <footer>
+        <div class="footSections">
+            <div class="footSection">
+                <h1>
+                    Contact Us
+                </h1>
+                <p><a href="">help@totallyscience.co</a></p>
+            </div>
+            <div class="footSection">
+                <h1>
+                    Our Team
+                </h1>
+                <div class="teamFoot">
+                    <div>
+                        Zach Yadegari
+                    </div>
+                    <div>
+                        ||
+                    </div>
+                    <div>
+                        Founder & CEO
+                    </div>
+                    <div>
+                        Ilan L
+                    </div>
+                    <div>
+                        ||
+                    </div>
+                    <div>
+                        Developer
+                    </div>
+                </div>
+            </div>
+            <div class="footSection">
+                <h1>
+                    Legal
+                </h1>
+                <p><a href="">Terms of Use</a></p>
+                <p><a href="">Privacy Policy</a></p>
+                <p><a href="">Partners</a></p>
+            </div>
+        </div>
+        <div class="bottomFoot">
+            © 2022 TotallyScience. A Zach Yadegari Production ™
+        </div>
+    </footer>
--- indextest.php
+++ indextest.php
@@ -46,66 +46,219 @@
         <p>Click <span style="text-decoration: underline"><b>here</b></span> to view alternate links</p>
     </div>
 
-    <div id="searchcat">
-        <div id="firstcatrow">
-            <button name="all" onclick="" class="selectedCategory categoryButton">All</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Some</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Couple</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Few</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Fun</button>
-            <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+    <div id="searchcatParent">
+        <div id="searchcat">
+            <div id="firstcatrow">
+                <button name="all" onclick="" class="selectedCategory categoryButton">All</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Some</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Couple</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Few</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Fun</button>
+                <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+            </div>
+            <div id="nextcatrow">
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
+                <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+            </div>
         </div>
-        <div id="nextcatrow">
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Week</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Month</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Not Fun</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Boring</button>
-            <button name="all" onclick="" class="unselectedCategory categoryButton">Time</button>
+
+
+        <div id="games">
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
+            <div id="gameDiv">
+                <input type="image"
+                    src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">Bottom Right</div>
+            </div>
         </div>
     </div>
 
-    <div id="games">
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        <div id="gameDiv">
-            <input type="image" src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
-            <div class="innerGameDiv border"></div>
-            <div class="innerGameDiv">Bottom Right</div>
-        </div>
-        
+    <div style="text-align:center">
+        <button onclick="" class="regularButton"><span>ALL GAMES</span> <img style="padding-left:0.5vw" width="16vw"
+                src="https://www.cooperlighting.com/s/vividtune/png/right-arrow.png"></button>
     </div>
 
+    <div id="extraCategories">
+        <div id="extraCategory">
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzjDjKTvljf2ZWNwwFDGJbRwCvmbWL9xKghA&usqp=CAU" />
+            </div>
+            <p>Henry Stickmin Games</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img
+                    src="https://play-lh.googleusercontent.com/myJ2kwywsCRglKkZuHBq6Aku9PX7HB75v-8KbnTK9JOPU6z0hpD9xtvjiTGqrn-e4eI" />
+            </div>
+            <p>Five Nights at Freddy's Games</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://i1.sndcdn.com/avatars-fKIWZo5CKgieiX3m-HeK5MA-t240x240.jpg" />
+            </div>
+            <p>Friday Night Funkin' Games</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://files.gamezhero.com/game/de4/d2c/de4d2c1e1953443a/images/240/vex.jpg" />
+            </div>
+            <p>Vex Games</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img
+                    src="https://lh3.googleusercontent.com/47RcwWXXnAimVJKSGsj-DKfIQKCwocry-TNO-m5G13eXEJM6NDoG6Y6uuhvzR7qehg" />
+            </div>
+            <p>Driving Games</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+            </div>
+            <p>Game</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+            </div>
+            <p>Game</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+            </div>
+            <p>Game</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img src="https://cdn.akamai.steamstatic.com/steam/apps/1251460/capsule_616x353.jpg?t=1607094043" />
+            </div>
+            <p>Game</p>
+        </div>
+        <div id="extraCategory">
+            <div>
+                <img
+                    src="https://www.freepnglogos.com/uploads/star-png/star-vector-png-transparent-image-pngpix-21.png" />
+            </div>
+            <p>Game</p>
+        </div>
+    </div>
+
+    <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
     <script src="assets/scripts/index.js"></script>
