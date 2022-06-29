9684ebe8ce78504b253d52b22d973372b08eeb0d
HackedZach <zay0106@icloud.com>
2022-06-29 16:22:10 -0400

    games loading

--- assets/scripts/indexnew.js
+++ assets/scripts/indexnew.js
@@ -37,3 +37,77 @@ autoSwitch()
 
 
 // Load Games
+const gamesDiv = document.getElementById('games');
+const maxGames = 50;
+
+let selectedTopic = 'all';
+let displayedGames = 0;
+let games;
+let sorted;
+let hasLoaded = false;
+
+
+let sortObject = (obj) =>
+    Object.keys(obj).sort().reduce((res, key) => ((res[key] = obj[key]), res), {})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`assets/games.json`).then((response) => response.json()).then((retrievedGames) => {
+        games = retrievedGames;
+        loadTopic();
+    });
+});
+
+function loadTopic() {
+    displayedGames = 0;
+    gamesDiv.innerHTML = '';
+
+    sorted = sortObject(games);
+
+    if (selectedTopic != 'all') {
+        let gamesWithTopic = {};
+
+        for (let game in sorted) {
+            if (sorted[game].tags.includes(selectedTopic)) {
+                gamesWithTopic[game] = sorted[game];
+            }
+        }
+
+        sorted = gamesWithTopic;
+    }
+
+    displayGames();
+}
+
+
+function displayGames() {
+    for (let x = displayedGames; x < displayedGames + maxGames; x++) {
+        let keys = Object.keys(sorted);
+
+        const name = keys[x];
+        const data = sorted[keys[x]];
+
+        let classlist = data.tags.join(' ');
+
+        const weekAgo = new Date();
+        weekAgo.setDate(weekAgo.getDate() - 7);
+
+        const gameDate = new Date(data.date_added);
+
+        if (gameDate > weekAgo) {
+            classlist += ' new';
+        }
+
+
+        const gameBtn = `
+            <div id="gameDiv" onclick="location.href = 'class?class=${name}'" class="${classlist} all">
+                <input type="image"
+                    src="${data.image}" />
+                <div class="innerGameDiv border"></div>
+                <div class="innerGameDiv">${name}</div>
+            </div>
+        `;
+
+        gamesDiv.innerHTML += gameBtn;
+    }
+}
