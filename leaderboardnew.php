4ce50e4a80c7153ad43688ef8ec92573b2d6e43d
HackedZach <zay0106@icloud.com>
2022-07-02 00:31:01 -0400

    fix fetch file address

--- leaderboardnew.php
+++ leaderboardnew.php
@@ -71,7 +71,7 @@ $conn->close();
 
     <script>
     console.log("Here");
-    fetch(`assets/php/leaderboardnew.php`).then((response) => response.text()).then((res) => {
+    fetch(`leaderboardnew.php`).then((response) => response.text()).then((res) => {
         res = JSON.parse(res);
 
         const highscores = res;
