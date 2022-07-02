c5ff93be823d9e11f3b045c5db394af320e2341f
HackedZach <zay0106@icloud.com>
2022-07-02 01:00:55 -0400

    idk

--- leaderboardnew.php
+++ leaderboardnew.php
@@ -33,37 +33,38 @@
         fetch(`assets/games.json`).then((response) => response.json()).then((retrievedGames) => {
             games = retrievedGames;
             console.log(games);
-        });
-        console.log(res);
+            console.log(res);
         
-        res = JSON.parse(res);
+            res = JSON.parse(res);
 
-        const highscores = res;
-        const scoresDiv = document.getElementById('highscores');
+            const highscores = res;
+            const scoresDiv = document.getElementById('highscores');
 
-        for (score in highscores) {
-            const game = highscores[score][0];
-            const name = highscores[score][1];
-            const gameScore = highscores[score][2];
-            const highscoreDiv = `
-                <div id="highscore" onclick="location.href = 'class?class=${game}'">
-                    <div class="text">
-                        <h1>
-                            ${game}
-                        </h1>
-                        <h2>
-                            ${gameScore}
-                        </h2>
-                        <p>
-                            ${name}
-                        </p>
+            for (score in highscores) {
+                const game = highscores[score][0];
+                const name = highscores[score][1];
+                const gameScore = highscores[score][2];
+                const highscoreDiv = `
+                    <div id="highscore" onclick="location.href = 'class?class=${game}'">
+                        <div class="text">
+                            <h1>
+                                ${game}
+                            </h1>
+                            <h2>
+                                ${gameScore}
+                            </h2>
+                            <p>
+                                ${name}
+                            </p>
+                        </div>
+                        <div class="image">
+                            <img src="${games[game].image}">
+                        </div>
                     </div>
-                    <div class="image">
-                        <img src="${games[game].image}">
-                    </div>
-                </div>
-            `;
-            scoresDiv.innerHTML += highscoreDiv;
+                `;
+                scoresDiv.innerHTML += highscoreDiv;
+            });
+        
         }
     });
     </script>
