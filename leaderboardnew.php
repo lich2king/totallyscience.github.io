90947b8420dfdce7d851eb17f6d4f6767c70831b
HackedZach <zay0106@icloud.com>
2022-07-03 18:24:24 -0400

    wef

--- leaderboardnew.php
+++ leaderboardnew.php
@@ -30,7 +30,7 @@
 
 
 
-    <script src="assets/scripts/leaderboard.js?v2"></script>
+    <script src="assets/scripts/leaderboard.js?v3"></script>
     <script src="assets/scripts/main.js"></script>
 </body>
 
