3c716cbd66c03e2d15f34350ed6b7ebcdcb05f1e
HackedZach <zay0106@icloud.com>
2022-07-03 18:29:37 -0400

    surive

--- leaderboardnew.php
+++ leaderboardnew.php
@@ -30,7 +30,7 @@
 
 
 
-    <script src="assets/scripts/leaderboard.js?v5"></script>
+    <script src="assets/scripts/leaderboard.js?v6"></script>
     <script src="assets/scripts/main.js"></script>
 </body>
 
