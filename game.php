7ba87fca0f456bbd7267a3aa0497058c424b8dbc
HackedZach <zay0106@icloud.com>
2022-07-05 14:37:18 -0400

    through the wire

---
+++ game.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/game.css?v3'>
+
+    <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+
+    <div id="gameframe">
+        <div class="game">
+        </div>
+        <div class="options">
+            <input type="image" id="image" alt="Login" src="https://www.pngkit.com/png/full/818-8189073_icon-02-thumbs-up-white-png.png">
+            <input type="image" id="image" alt="Login" src="https://www.nicepng.com/png/full/37-375088_icons-health-trophy-trophy-icon-white-png.png">
+            <input type="image" id="image" alt="Login" src="/assets/images/icons/saveicon.png">
+        </div>
+    </div>
+
+
+    <?php include "assets/includes/footer.php" ?>
+
+
+
+    <script src="assets/scripts/game.js?v13"></script>
+    <script src="assets/scripts/main.js"></script>
+</body>
+
+</html>
