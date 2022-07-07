f1bf976c51b04b44b593d76346631c3a599e450a
HackedZach <zay0106@icloud.com>
2022-07-07 14:25:06 -0400

    chat start of page

---
+++ chatnew.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/chat.css'>
+
+    <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+    <div id="chatoptions">
+        <input type="text" id="roomcode" placeholder="Room Code...">
+        <br>
+        <button onclick="window.open('gamesnew.php', '_self')" class="regularButton"><span>Join Room</span></button>
+        <br>
+        <button onclick="window.open('gamesnew.php', '_self')" class="regularButton"><span>Create Room</span></button>
+    </div>
+
+
+    <?php include "assets/includes/footer.php" ?>
+
+    <script src="assets/scripts/main.js"></script>
+    <script src="assets/scripts/chatnew.js"></script>
+</body>
+
+</html>
