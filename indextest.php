1ec78d9a284d58e94304c93515af3a04507c5434
HackedZach <zay0106@icloud.com>
2022-07-08 15:18:34 -0400

    efwef

--- indextest.php
+++ indextest.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include "assets/includes/head.php" ?>
-    <link rel='stylesheet' href='assets/styles/indextest.css?v4'>
+    <link rel='stylesheet' href='assets/styles/indextest.css?v5'>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -170,7 +170,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/indexnew.js?v1"></script>
+    <script src="assets/scripts/indexnew.js?v2"></script>
 </body>
 
 </html>
