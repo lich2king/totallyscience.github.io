6f43fc5565a846dfae39930705cce714c028ffc6
HackedZach <zay0106@icloud.com>
2022-07-08 14:53:43 -0400

    appnew file created

---
+++ appnew.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/appnew.css'>
+
+    <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+    <div id="apps">
+
+    </div>
+
+    <?php include "assets/includes/footer.php" ?>
+
+    <script src="assets/scripts/main.js"></script>
+    <script src="assets/scripts/appnew.js"></script>
+</body>
+
+</html>
