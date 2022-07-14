dbf2d36cb11cb2fb028dd71efaa2625188c009a6
ssl <77415460+sslprograms@users.noreply.github.com>
2022-07-14 13:47:31 -0700

    still working on

---
+++ gamepass.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/gamepass.css'>
+
+    <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
+</head>
+
+<body id="mainBody">
+    <?php include "assets/includes/navbar.php" ?>
+
+    <div class="gamepass-header">
+        <h1>GAME PASS</h1>
+    </div>
+
+    <div class="product-container">
+
+        <div class="p-view">
+            <h1>FREE</h1>
+            <h3>$0 / Month</h3>
+            <p>Upgrade whenever you want Free access to all our base features</p>
+            <button class="btn">Join</button>
+        </div>
+
+        <div class="p-view">
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
