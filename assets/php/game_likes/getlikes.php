77a648f6b3bd82928cb9d87ef928b64d323f3fb8
HackedZach <zay0106@icloud.com>
2022-08-10 15:03:13 -0400

    get like count

---
+++ assets/php/game_likes/getlikes.php
@@ -0,0 +1,26 @@
+<?php
+include 'config.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+  
+// Check connection
+if ($conn->connect_error) {
+  die("connection failed"); //. $conn->connect_error);
+}
+
+$gameName = htmlspecialchars($_GET["name"]);
+
+
+$query = "SELECT * FROM liked_games WHERE game = '$gameName'";
+$result = mysqli_query($conn, $query);
+
+
+
+echo(mysqli_num_rows($result));    
+
+
+   
+$conn->close();
+
+?>
