2bb7c7e0c1ee03edb643866eaa82b5961b870130
HackedZach <zay0106@icloud.com>
2022-08-10 15:10:49 -0400

    ye

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,7 +1,8 @@
 <?php
     // READY
-    $servername = "localhost";
-    $username = "u483325885_user";
-    $password = "Totally_password4321";
-    $database = "u483325885_database";
+    // $servername = "localhost";
+    // $username = "u483325885_user";
+    // $password = "Totally_password4321";
+    // $database = "u483325885_database";
+    die("Hi");
 ?>
