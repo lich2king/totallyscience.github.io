ce159cf6e59e4cb963374e664f07f3a98b920539
HackedZach <zay0106@icloud.com>
2022-08-09 23:55:55 -0400

    basketball stars is weird

--- game.php
+++ game.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/game.css?v4'>
+    <link rel='stylesheet' href='assets/styles/game.css?v5'>
 
     <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
 </head>
