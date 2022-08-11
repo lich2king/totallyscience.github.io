c7bc1a2d126f6a5ac15512a12f2a840b842faa51
HackedZach <zay0106@icloud.com>
2022-08-11 16:28:27 -0400

    wef

--- assets/php/game_pin/pingame.php
+++ assets/php/game_pin/pingame.php
@@ -41,7 +41,7 @@ else
     //game is not pinned
     if(substr_count($result,";") < 3)
     {
-        $query = "UPDATE `accounts` SET PinnedGames='$result$gameName;' WHERE ID='$userid'";
+        $query = "UPDATE `accounts` SET PinnedGames='$gameName;' WHERE ID='$userid'";
         mysqli_query($conn, $query);
         echo('successpinned');
         // if ($conn->query($query) === TRUE) 
