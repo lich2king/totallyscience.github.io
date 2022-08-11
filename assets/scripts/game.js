ff06b22a323b0b6d6638e1567a25fdd74e878d49
HackedZach <zay0106@icloud.com>
2022-08-10 23:27:06 -0400

    like count will auto format number with k's and ms

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -110,7 +110,7 @@ likeButton.addEventListener("webkitAnimationEnd", function() {
 
 
 function UpdateLikeCount() {
-    document.getElementById('likeCount').innerText = likeCount;
+    document.getElementById('likeCount').innerText = numFormatter(likeCount);
 }
 
 
@@ -246,3 +246,16 @@ var randomProperty = function(object) {
     var keys = Object.keys(object);
     return keys[Math.floor(keys.length * Math.random())];
 };
+
+// converts number to string representation with K and M.
+// toFixed(d) returns a string that has exactly 'd' digits
+// after the decimal place, rounding if necessary.
+function numFormatter(num) {
+    if (num > 999 && num < 1000000) {
+        return (num / 1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million 
+    } else if (num < 1000) {
+        return num; // if value < 1000, nothing to do
+    }
+}
