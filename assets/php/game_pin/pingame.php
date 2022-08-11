1dac89dabc4989b882e17d17d7d116e7fbeaee6e
HackedZach <zay0106@icloud.com>
2022-08-11 16:19:23 -0400

    s

--- assets/php/game_pin/pingame.php
+++ assets/php/game_pin/pingame.php
@@ -43,7 +43,7 @@ else
     {
         $query = "UPDATE `accounts` SET `PinnedGames`='$gameName;' WHERE ID='$userid'";
         mysqli_query($conn, $query);
-        echo('succwwesspinned');
+        echo('succwwesspissnned');
         // if ($conn->query($query) === TRUE) 
         // {
         //     echo('successpinned');
