ee04403afb75c9be5c584b9852c5421302a08d95
HackedZach <zay0106@icloud.com>
2022-08-11 15:23:17 -0400

    we

--- assets/php/game_pin/pingame.php
+++ assets/php/game_pin/pingame.php
@@ -29,7 +29,7 @@ if (!function_exists('str_contains')) {
     }
 }
 
-die(str_contains('Slope;Chess;', `Tanuki Sunset;`));
+die(str_contains('Slope;Chess;', `Chess;`));
 
 if(str_contains($result, `$gameName;`))
 {
