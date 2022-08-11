80df2c7156fe2da25cbf72313b2c4472912f6508
HackedZach <zay0106@icloud.com>
2022-08-11 15:27:51 -0400

    wef

--- assets/php/game_pin/pingame.php
+++ assets/php/game_pin/pingame.php
@@ -29,7 +29,7 @@ if (!function_exists('str_contains')) {
     }
 }
 
-die(str_contains('Slope;Chess;', `Slope;`));
+die(str_contains('Slope;Chess;', 'Slope;'));
 
 if(str_contains($result, `$gameName;`))
 {
