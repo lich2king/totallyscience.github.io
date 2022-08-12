b1e9ea20de40a656ca0d545c874cccbcaf8c0965
HackedZach <zay0106@icloud.com>
2022-08-12 12:48:01 -0400

    hmm

--- assets/scripts/indexnew.js
+++ assets/scripts/indexnew.js
@@ -217,7 +217,7 @@ function suggestGames() {
             randomGames.push(randGame);
         }
 
-        alert(pinnedGames);
+        alert(pinnedGames.length);
         if (pinnedGames.length < 3) {
             let generateGames = 3 - pinnedGames.length;
             for (let i = 0; i < generateGames + 1; i++) {
