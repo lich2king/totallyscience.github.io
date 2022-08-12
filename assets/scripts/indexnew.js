aafa9c7c5d06bc922b696cde398a7abdbbc5fed8
HackedZach <zay0106@icloud.com>
2022-08-12 17:20:10 -0400

    pinned games pin

--- assets/scripts/indexnew.js
+++ assets/scripts/indexnew.js
@@ -244,6 +244,7 @@ function suggestGames() {
             game = pinnedGames[i];
             gameBtn = `
                     <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${games[game]["image"]})" id="gameDiv" onclick="location.href = 'game.php?class=${encodeURIComponent(game)}'">
+                        <button id="pin"><img src="/assets/images/icons/coloredpin.png"></button>
                         <div class="innerGameDiv">${game}</div>
                     </div>
                     `;
