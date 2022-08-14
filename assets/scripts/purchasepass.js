d5ce09a68503eaa19c977f96c553344fdd0d3e65
HackedZach <zay0106@icloud.com>
2022-08-14 00:10:46 -0400

    efw

---
+++ assets/scripts/purchasepass.js
@@ -0,0 +1,16 @@
+var payplan = document.getElementById("plans");
+
+
+payplan.addEventListener("change", function() {
+    if (payplan.value == "monthly") {
+        document.getElementById("description").innerText = "$4.99/month membership. Cancel Anytime";
+
+        document.getElementById("monthpaypal").style.display = '';
+        document.getElementById("annualpaypal").style.display = 'none';
+    } else if (payplan.value == "annual") {
+        document.getElementById("description").innerText = "$47.88 billed once. GamePass for one year";
+
+        document.getElementById("monthpaypal").style.display = 'none';
+        document.getElementById("annualpaypal").style.display = '';
+    }
+});
--- purchasepass.php
+++ purchasepass.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/gamepass.css'>
+    <link rel='stylesheet' href='assets/styles/purchasepass.css'>
 
     <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
 </head>
@@ -16,153 +16,59 @@
         <h1>GAME <span style="color: var(--accent-color)">PASS</span></h1>
     </div>
 
-    <div class="product-container">
-        <div class="p-view">
-            <div class="c-item">
-                <h1>Monthly</h1>
-                <h3>$4.99 <span>/ month</span>
-                    <p style="font-weight:lighter; padding:0; margin-top:0;">Paid each month</p>
-                </h3>
-                <div style="display: flex; flex: 1; align-content: center; justify-content:center;margin-top:0;">
-                    <button onclick="location.href = 'premiumpass.php'" class="btn">Subscribe</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="p-view">
-            <div class="c-item">
-                <h1>Annual</h1>
-                <h3>$3.99 <span>/ month</span>
-                    <p style="font-weight:lighter; padding:0; margin-top:0;">Billed once at $47.88</p>
-                </h3>
-                <div style="display: flex; flex: 1; align-content: center; justify-content:center;margin-top:0;">
-                    <button onclick="location.href = 'premiumpass.php'" class="btn">Purchase</button>
-                </div>
-                <button id="pin"><img src="assets/images/icons/discount.png"></button>
-            </div>
-        </div>
-
+    <div class="details-container">
+        <h2>Payment Plan</h2>
+        <select name="paymentplans" id="plans">
+            <option value="monthly">Monthly - Paid each month</option>
+            <option value="annual">Annual - One payment for the year</option>
+        </select>
+        <p id="description">
+            $4.99/month membership. Cancel Anytime
+        </p>
     </div>
 
-    <div class="whygoprem-container">
-        <h1>Why Get Game Pass?</h1>
-        <div class="why-rows">
-            <div class="reason">
-                <img class="img-label" src="assets/images/icons/lock.png" alt="">
-                <h1>MORE GAMES</h1>
-                <p>Unlock many games that are exclusive to premium members</p>
-            </div>
 
-            <div class="reason">
-                <img class="img-label" src="assets/images/icons/eye.png" alt="">
-                <h1>PRIVATE LINKS</h1>
-                <p>Access a list of private links only premium members can use</p>
-            </div>
-
-            <div class="reason">
-                <img class="img-label" src="assets/images/icons/block-ads.png" alt="">
-                <h1>AD-FREE</h1>
-                <p>Your experience will no longer be interrupted by ads</p>
-            </div>
+    <div id="payment-container">
+        <div class="payment" id="monthpaypal">
+            <div id="paypal-button-container-P-22399174D2019973RML4B54Q"></div>
+            <script
+                src="https://www.paypal.com/sdk/js?client-id=AVd4hxcHkYm85fyGxB5Fg7IcrpZ_l1cxb_QSmrVeStRo90-biqKgpaIIfxtcbmfZd2ejFxV7iVEEc1ix&vault=true&intent=subscription"
+                data-sdk-integration-source="button-factory"></script>
+            <div id="paypal-button-container-P-22399174D2019973RML4B54Q"></div>
+            <script
+                src="https://www.paypal.com/sdk/js?client-id=AVd4hxcHkYm85fyGxB5Fg7IcrpZ_l1cxb_QSmrVeStRo90-biqKgpaIIfxtcbmfZd2ejFxV7iVEEc1ix&vault=true&intent=subscription"
+                data-sdk-integration-source="button-factory"></script>
+            <script>
+            paypal.Buttons({
+                style: {
+                    shape: 'pill',
+                    color: 'blue',
+                    layout: 'vertical',
+                    label: 'paypal'
+                },
+                createSubscription: function(data, actions) {
+                    return actions.subscription.create({
+                        /* Creates the subscription */
+                        plan_id: 'P-22399174D2019973RML4B54Q'
+                    });
+                },
+                onApprove: function(data, actions) {
+                    alert(data
+                    .subscriptionID); // You can add optional success message for the subscriber here
+                }
+            }).render('#paypal-button-container-P-22399174D2019973RML4B54Q'); // Renders the PayPal button
+            </script>
         </div>
-    </div>
-
-    <div class="whygoprem-container" style="padding-bottom: 5%">
-        <h1>Frequently Asked Questions</h1>
-        <div class="question-container">
-            <div class="question">
-                <p>What’s included with Game Pass?</p>
-                <input type="image" id="image" src="/assets/images/icons/down_arrow.png"
-                    onclick="dropDownFaq('included')">
-            </div>
-            <div class="answer" style="display: none" id="included">
-                <div>
-                    <ul>
-                        <li>You have access to games locked for free pass users</li>
-                        <br>
-                        <li>A list of private links only accessible to users with game pass</li>
-                        <br>
-                        <li>You will not have the annoyance of ads</li>
-                        <br>
-                        <li>You have the ability to save progress in games while playing them from different links or on
-                            different computers</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="question">
-                <p>Why would I want access to Private Links?</p>
-                <input type="image" id="image" src="/assets/images/icons/down_arrow.png"
-                    onclick="dropDownFaq('private')">
-            </div>
-            <div class="answer" style="display: none" id="private">
-                <div>
-                    <ul>
-                        <li>Private links are only accessible to premium users, so it is much more unlikely for a
-                            private link to get caught and blocked in your school</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="question">
-                <p>How do I cancel my Premium Game Pass Subscription?</p>
-                <input type="image" id="image" src="/assets/images/icons/down_arrow.png"
-                    onclick="dropDownFaq('cancel')">
-            </div>
-            <div class="answer" style="display: none" id="cancel">
-                <div>
-                    <ul>
-                        <li>You can cancel your subscription by clicking the link <a href="">here</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="question">
-                <p>Could I pay for the full year and not monthly?</p>
-                <input type="image" id="image" src="/assets/images/icons/down_arrow.png" onclick="dropDownFaq('year')">
-            </div>
-            <div class="answer" style="display: none" id="year">
-                <div>
-                    <ul>
-                        <li>Yes, you can choose to pay annually instead of monthly</li>
-                    </ul>
-                </div>
-            </div>
+        <div class="payment" id="annualpaypal">
+            
         </div>
-    </div>
-
-    <div class="product-container">
-
-        <div class="p-view">
-            <div class="c-item">
-                <h1>Monthly</h1>
-                <h3>$4.99 <span>/ month</span>
-                    <p style="font-weight:lighter; padding:0; margin-top:0;">Paid each month</p>
-                </h3>
-                <div style="display: flex; flex: 1; align-content: center; justify-content:center;margin-top:0;">
-                    <button onclick="location.href = 'premiumpass.php'" class="btn">Subscribe</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="p-view">
-            <div class="c-item">
-                <h1>Annual</h1>
-                <h3>$3.99 <span>/ month</span>
-                    <p style="font-weight:lighter; padding:0; margin-top:0;">Billed once at $47.88</p>
-                </h3>
-                <div style="display: flex; flex: 1; align-content: center; justify-content:center;margin-top:0;">
-                    <button onclick="location.href = 'premiumpass.php'" class="btn">Purchase</button>
-                </div>
-                <button id="pin"><img src="assets/images/icons/discount.png"></button>
-            </div>
-        </div>
-    </div>
-
     </div>
 
 </body>
 
 
 <?php include "assets/includes/footer.php" ?>
-<script src="assets/scripts/gamepass.js"></script>
+<script src="assets/scripts/purchasepass.js"></script>
 <script src="assets/scripts/main.js"></script>
 
 </html>
