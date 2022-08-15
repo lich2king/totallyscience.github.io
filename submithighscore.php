05513eed3a4406a838dfbb59aade7873c7ef0351
HackedZach <zay0106@icloud.com>
2022-08-15 16:37:37 -0400

    submit highscore?

---
+++ submithighscore.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/submithighscore.css'>
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+    <div id="gameInfo">
+        <p>What game is it for?</p>
+        <select name="gamesSelect" id="gamesSelect">
+        </select>
+
+        <p>What is your score?</p>
+        <input type="text" id="scoreText" placeholder="100">
+
+        <p>Submit a screenshot of your score (Show full screen including top website bar otherwise INVALID)</p>
+        <input type="file" id="screenshot" placeholder="">
+        <br><br>
+        <button class="button" id="save-button" onclick="SubmitHighscore()">Submit</button>
+        <p style="text-align: center; color: red;" id="errorText"></p>
+    </div>
+
+    <?php include "assets/includes/footer.php" ?>
+
+    <script src="assets/scripts/main.js"></script>
+    <script src="assets/scripts/submithighscore.js"></script>
+</body>
+
+</html>
