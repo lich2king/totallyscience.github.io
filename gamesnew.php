8d077d23802b7f25147c9acd9541293190b860d7
HackedZach <zay0106@icloud.com>
2022-08-17 21:40:24 -0400

    wef

--- gamesnew.php
+++ gamesnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/gamesnew.css'>
+    <link rel='stylesheet' href='assets/styles/gamesnew.css?v1'>
 
     <!--Ctrl + Alt + F (html in PHP formatting)         Ctrl+Alt+arrow (select multiple lines)-->
 </head>
