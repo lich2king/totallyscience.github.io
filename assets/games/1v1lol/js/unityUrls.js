d7e016e5a6304460e2e50ef552e23d93951c1972
HackedZach <zay0106@icloud.com>
2022-08-18 00:26:58 -0400

    new games

---
+++ assets/games/1v1lol/js/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getGameJsonUrl() {
+	return "WebGL.json";
+}
+
+function getGameLoaderUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/8/85164e58ab08443f8040e19ff4175bd8/UnityLoader.js";
+}
