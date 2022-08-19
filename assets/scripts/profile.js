7e0b84b59a396290c15d1f70fd47dfff8b53a5f0
HackedZach <zay0106@icloud.com>
2022-08-19 16:47:03 -0400

    show your highscores

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             <div class="highscore">
                                 <img src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/70434/trophy-clipart-xl.png">
                                 <h1>${game}</h1>
-                                <p>${gameScore}</p>
+                                <p>${numFormatter(gameScore)}</p>
                             </div>
                         `;
                 scoresDiv.innerHTML += highscoreDiv;
@@ -76,4 +76,17 @@ function logout() {
     fetch(`assets/php/logout.php`).then(() => {
         location.href = 'login.php';
     });
+}
+
+// converts number to string representation with K and M.
+// toFixed(d) returns a string that has exactly 'd' digits
+// after the decimal place, rounding if necessary.
+function numFormatter(num) {
+    if (num > 999 && num < 1000000) {
+        return (num / 1000).toFixed(1) + 'K'; // convert to K for number from > 1000 < 1 million 
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(1) + 'M'; // convert to M for number from > 1 million 
+    } else if (num < 1000) {
+        return num; // if value < 1000, nothing to do
+    }
 }
--- profilenew.php
+++ profilenew.php
@@ -133,7 +133,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/profile.js?v20"></script>
+    <script src="assets/scripts/profile.js?v19"></script>
     <script>
     
     </script>
