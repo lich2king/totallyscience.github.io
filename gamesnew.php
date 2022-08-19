8046b37cb6f3b4c7b36e1b87ad73c0ab8a359fd0
HackedZach <zay0106@icloud.com>
2022-08-19 18:20:35 -0400

    wef

--- gamesnew.php
+++ gamesnew.php
@@ -62,7 +62,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/gamesnew.js?v6"></script>
+    <script src="assets/scripts/gamesnew.js?v7"></script>
 </body>
 
 </html>
