9702507bddf6e7e7fd886da063a4c41dc3caeb56
HackedZach <zay0106@icloud.com>
2022-08-19 16:25:55 -0400

    wf

--- profilenew.php
+++ profilenew.php
@@ -143,7 +143,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/profile.js?v8"></script>
+    <script src="assets/scripts/profile.js?v9"></script>
     <script>
     
     </script>
