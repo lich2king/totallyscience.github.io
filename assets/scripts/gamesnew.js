21a15a9e109c07f67c4a032c810c8e1ce016a583
HackedZach <zay0106@icloud.com>
2022-08-20 19:25:04 -0400

    wef

--- assets/scripts/gamesnew.js
+++ assets/scripts/gamesnew.js
@@ -114,31 +114,38 @@ async function displayGames() {
     await fetch(`/assets/php/game_likes/personallikes.php`).then((response) => response.text()).then((res) => {
         var likedgames = JSON.parse(res);
 
-        await fetch(`/assets/php/recent_games/recentgames.php`).then((response) => response.text()).then((res) => {
-            let recentGames = res.split(";");
-            recentGames = recentGames.slice(1);
-            const recentContainer = document.getElementById("recentContainer");
+        Array.from(gameButtons).forEach(game => {
+            let liked = false;
+            for (like in likedgames) {
+                if (likedgames[like][0] == game.getAttribute("name")) {
+                    liked = true;
+                }
+            }
 
-            Array.from(gameButtons).forEach(game => {
-                let liked = false;
-                let recent = false;
-                for (like in likedgames) {
-                    if (likedgames[like][0] == game.getAttribute("name")) {
-                        liked = true;
-                    }
+            if (liked) {
+                game.classList.add('liked');
+            }
+
+        });
+
+    });
+    await fetch(`/assets/php/recent_games/recentgames.php`).then((response) => response.text()).then((res) => {
+        let recentGames = res.split(";");
+        recentGames = recentGames.slice(1);
+        const recentContainer = document.getElementById("recentContainer");
+
+        Array.from(gameButtons).forEach(game => {
+            let recent = false;
+
+            for (let i = 0; i < recentGames.length; i++) {
+                if (recentGames[i] == game.getAttribute("name")) {
+                    recent = true;
                 }
-                for (let i = 0; i < recentGames.length; i++) {
-                    if (recentGames[i] == game.getAttribute("name")) {
-                        recent = true;
-                    }
-                }
-                if (liked) {
-                    game.classList.add('liked');
-                }
-                if (recent) {
-                    game.classList.add('recent');
-                }
-            });
+            }
+
+            if (recent) {
+                game.classList.add('recent');
+            }
         });
     });
 }
