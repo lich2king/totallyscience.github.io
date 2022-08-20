8ac59df5550ab5884a9f96b28cf4e287b0a78a5e
HackedZach <zay0106@icloud.com>
2022-08-20 18:24:41 -0400

    wef

--- game.php
+++ game.php
@@ -88,7 +88,7 @@
 
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/game.js?v40wef"></script>
+    <script src="assets/scripts/game.js?v41"></script>
 
 </body>
 
