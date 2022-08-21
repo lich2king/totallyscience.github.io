e6c17a75e5d44a93417362644ca8c6de2c8711be
HackedZach <zay0106@icloud.com>
2022-08-21 00:23:53 -0400

    wef

--- indextest.php
+++ indextest.php
@@ -181,7 +181,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/indexnew.js?v40"></script>
+    <script src="assets/scripts/indexnew.js?v41"></script>
 </body>
 
 </html>
