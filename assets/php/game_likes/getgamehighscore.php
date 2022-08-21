1ebb903071700da1cc7f1500dde2c3ed498a6257
HackedZach <zay0106@icloud.com>
2022-08-21 16:03:59 -0400

    you do not need to be logged in to view highscores

--- assets/php/game_likes/getgamehighscore.php
+++ assets/php/game_likes/getgamehighscore.php
@@ -12,11 +12,6 @@ if ($conn->connect_error) {
 
 $gameName = htmlspecialchars($_GET["name"]);
 
-if (!isset($_COOKIE['logintoken'])) {
-    die("no cookie");
-}
-
-$userid = json_decode($_COOKIE['logintoken'], true)['id'];
 
 $query = "SELECT score FROM `highscores` WHERE game='$gameName'";
 $result = mysqli_query($conn, $query);
