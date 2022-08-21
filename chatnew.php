aaf2e368adf3a5a2bbe0750936ca6e44679e786d
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-21 17:58:53 -0400

    cache

--- chatnew.php
+++ chatnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v1'>
 </head>
 
 <body>
