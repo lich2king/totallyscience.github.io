8b7e7b3fd3ef7b6379c2c4f631d5dac6f44279bd
HackedZach <zay0106@icloud.com>
2022-08-22 19:24:58 -0400

    wer

--- assets/php/addgameviews.php
+++ assets/php/addgameviews.php
@@ -1,4 +1,5 @@
 <?php
+
 include './config.php';
 
 // Create connection
@@ -9,10 +10,33 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
+$gameName = htmlspecialchars($_GET["name"]);
 
-$query = "UPDATE `game_data` SET `totalviews`=`totalviews`+1,`day1`=`day1`+1 WHERE game='Slope'";
-$result = mysqli_query($conn, $query);
 
+
+
+//first check if the user has not already liked the game
+
+$query = "SELECT * FROM game_data WHERE game = '$gameName'";
+$result = mysqli_num_rows(mysqli_query($conn, $query));
+
+if($result <= 0)
+{
+    //game does not exist in database
+    $query = "INSERT INTO game_data (game, totalviews, day1)
+    VALUES ('$gameName', 1, 1)";
+
+    $result = mysqli_query($conn, $query);
+}
+else
+{   
+    //game exists in database
+    $query = "UPDATE `game_data` SET `totalviews`=`totalviews`+1,`day1`=`day1`+1 WHERE game='$gameName'";
+    $result = mysqli_query($conn, $query);
+}
+
+
+   
 $conn->close();
 
 ?>
