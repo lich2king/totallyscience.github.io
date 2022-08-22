20770872e9f39a0faf17df46ec09598c2a525653
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-22 19:21:17 -0400

    testing testing 123

--- assets/php/addgameviews.php
+++ assets/php/addgameviews.php
@@ -13,4 +13,6 @@ if ($conn->connect_error) {
 $query = "UPDATE `game_data` SET `totalviews`=`totalviews`+1,`day1`=`day1`+1 WHERE game='Slope'";
 $result = mysqli_query($conn, $query);
 
+$conn->close();
+
 ?>
