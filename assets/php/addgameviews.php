aece200168018fa62304fe88447f2a4e4b5e097e
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-22 19:14:55 -0400

    revert

--- assets/php/addgameviews.php
+++ assets/php/addgameviews.php
@@ -9,7 +9,6 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
-echo "slope";
 
 $query = "UPDATE `game_data` SET `totalviews`=`totalviews`+1,`day1`=`day1`+1 WHERE game='Slope'";
 $result = mysqli_query($conn, $query);
