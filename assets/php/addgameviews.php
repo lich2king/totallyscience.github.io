0d8ae638a9068fc310539c4b0c33934e937501ab
HackedZach <zay0106@icloud.com>
2022-08-22 19:08:03 -0400

    wef

--- assets/php/addgameviews.php
+++ assets/php/addgameviews.php
@@ -3,8 +3,15 @@ include './config.php';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
+  
+// Check connection
+if ($conn->connect_error) {
+  die("connection failed"); //. $conn->connect_error);
+}
+
+$gameName = htmlspecialchars($_GET["name"]);
 
 $query = "UPDATE `game_data` SET `totalviews`=`totalviews`+1,`day1`=`day1`+1 WHERE game='$gameName'";
-    $result = mysqli_query($conn, $query);
+$result = mysqli_query($conn, $query);
 
 ?>
