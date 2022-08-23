5c09ee8202e71cf55e12d2b019157eb848d88866
HackedZach <zay0106@icloud.com>
2022-08-23 18:04:02 -0400

    wef

--- assets/scripts/indexnew.js
+++ assets/scripts/indexnew.js
@@ -127,7 +127,7 @@ function displayGames() {
         gamesDiv.innerHTML += gameBtn;
     }
 
-    await fetch(`/assets/php/getpopulargames.php`).then((response) => response.text()).then((res) => {
+    fetch(`/assets/php/getpopulargames.php`).then((response) => response.text()).then((res) => {
         let popularGames = JSON.parse(res);
 
         for (let i = 0; i < 10; i++) {
@@ -137,19 +137,6 @@ function displayGames() {
         }
     });
 
-    let loggedIn = false;
-
-    await fetch(`assets/php/getCookie.php?cookiename=logintoken`).then((response) => response.text()).then((res) => {
-        // HAVE RESEND EMAIL BUTTON
-        res = JSON.parse(res)
-        if (res != null) {
-            const isLoggedIn = res['isLoggedIn'];
-
-            if (isLoggedIn == 'true') {
-                loggedIn = true;
-            }
-        }
-    });
 
     //all games are generated... now add the liked and recent tags to the games
     const gameButtons = document.getElementsByClassName("all");
