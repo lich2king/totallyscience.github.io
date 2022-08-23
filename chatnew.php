5a96de150e0f7e046b38a04b807dde6ad69ad279
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-22 20:39:52 -0400

    odd list items diff color

--- chatnew.php
+++ chatnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v6'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v7'>
 </head>
 
 <body>
