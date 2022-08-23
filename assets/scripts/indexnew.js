6786d0568878baf4cd8d4a0939b76d43d09e2c01
HackedZach <zay0106@icloud.com>
2022-08-23 18:56:19 -0400

    game button creation changed

--- assets/scripts/indexnew.js
+++ assets/scripts/indexnew.js
@@ -397,59 +397,45 @@ function createGameButton(game, pin) {
 
     const gameDate = new Date(data.date_added);
 
-    if (gameDate > weekAgo) {
-        classlist += ' new';
+    let gameBtn = '';
+
+    let buttons = '';
+
+    if (data.tags.includes("gamepass")) {
+        buttons += "<button id='gamelock'><img src='/assets/images/icons/locked.png'></button>"
     }
 
-    let gameBtn = '';
-    if (data.tags.includes("gamepass") && pin != "hidden" && pin != "pin") {
-        gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${data.image})" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist} all">
-            <button id="gamelock"><img src="/assets/images/icons/locked.png"></button> 
-            <div class="innerGameDiv">${game}</div>
-        </div>
-        `;
-    } else if (data.tags.includes("gamepass") && pin == "hidden") {
-        gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="display:none" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist} all">
-            <button id="gamelock"><img src="/assets/images/icons/locked.png"></button> 
-            <div class="innerGameDiv">${game}</div>
-        </div>
-        `;
-    } else if (data.tags.includes("gamepass") && pin == "pin") {
-        gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${data.image})" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist}">
-            <button id="gamelock"><img src="/assets/images/icons/locked.png"></button>
-            <button id="pin"><img src="/assets/images/icons/coloredpin.png"></button> 
-            <div class="innerGameDiv">${game}</div>
-        </div>
-        `;
-    } else if (pin == "pin") {
+    if (pin == "pin") {
+        buttons += "<button id='pin'><img src='/assets/images/icons/coloredpin.png'></button>"
+    }
+
+
+    if (gameDate > weekAgo) {
+        classlist += ' new';
+        buttons += "<button id='newbanner'><img src='/assets/images/icons/newbanner.png'></button>"
+    }
+
+    if (pin != "suggested") {
+        classlist += ' all';
+    }
+
+
+    if (pin != "hidden") {
         gameBtn = `
         <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${data.image})" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist}">
-            <button id="pin"><img src="/assets/images/icons/coloredpin.png"></button>
-            <div class="innerGameDiv">${game}</div>
-        </div>
-        `;
-    } else if (pin == "suggested") {
-        gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${data.image})" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist}">
-            <div class="innerGameDiv">${game}</div>
-        </div>
-        `;
-    } else if (pin == "hidden") {
-        gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="display:none" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist} all">
+            ${buttons}
             <div class="innerGameDiv">${game}</div>
         </div>
         `;
     } else {
         gameBtn = `
-        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="background-image: url(${data.image})" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist} all">
+        <div onmouseout="(noGif(this));" onmouseover="changeToGif(this);" name="${game}" style="display: none" id="gameDiv" onclick="location.href = 'game.php?class=${game}'" class="${classlist}">
+            ${buttons}
             <div class="innerGameDiv">${game}</div>
         </div>
         `;
     }
+
 
     return (gameBtn);
 
