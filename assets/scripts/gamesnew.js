8df02e0980248411b37d6505c6d1abcbcf05d438
HackedZach <zay0106@icloud.com>
2022-08-22 23:47:50 -0400

    wef

--- assets/scripts/gamesnew.js
+++ assets/scripts/gamesnew.js
@@ -130,7 +130,9 @@ async function displayGames() {
     console.log("here");
     await fetch(`/assets/php/getpopulargames.php`).then((response) => response.text()).then((res) => {
         let popularGames = JSON.parse(res);
-
+        console.log(popularGames);
+        console.log(popularGames[0]);
+        console.log(popularGames[0][0]);
         for (let i = 0; i < 10; i++) {
             if (document.getElementsByName(popularGames[i][0])) {
                 document.getElementsByName(popularGames[i][0])[0].classList.add('popular');
