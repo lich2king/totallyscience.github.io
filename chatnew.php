d2c08d3b373297a4299b3e90b58ea1ec520653cc
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-22 20:23:36 -0400

    cache

--- chatnew.php
+++ chatnew.php
@@ -33,7 +33,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/chat.js?v8"></script>
+    <script src="assets/scripts/chat.js?v9"></script>
 </body>
 
 </html>
