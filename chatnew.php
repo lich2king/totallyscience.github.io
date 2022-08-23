c5cf9e5b0d0f122268d5bf9028d9e339349e28b0
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-22 20:45:22 -0400

    leave button color

--- chatnew.php
+++ chatnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v7'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v8'>
 </head>
 
 <body>
