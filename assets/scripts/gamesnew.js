b41fcb296f72adaa76fc3a8704142543f20c1a5a
HackedZach <zay0106@icloud.com>
2022-08-23 18:01:10 -0400

    wef

--- assets/scripts/gamesnew.js
+++ assets/scripts/gamesnew.js
@@ -117,7 +117,7 @@ async function displayGames() {
 
     await fetch(`assets/php/getCookie.php?cookiename=logintoken`).then((response) => response.text()).then((res) => {
         // HAVE RESEND EMAIL BUTTON
-        res = JSON.parse(res)
+        res = JSON.parse(res);
         if (res != null) {
             const isLoggedIn = res['isLoggedIn'];
 
