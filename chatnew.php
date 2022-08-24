257a194b5824278a3ff12ca80d684db963b65c9d
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-23 22:33:41 -0400

    tes

--- chatnew.php
+++ chatnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v10'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v11'>
 </head>
 
 <body>
