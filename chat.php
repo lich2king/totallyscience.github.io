c700d728a47ef9fa31fe02633c962f8725f9811e
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-24 14:53:33 -0400

    styling

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v13'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v14'>
 </head>
 
 <body>
