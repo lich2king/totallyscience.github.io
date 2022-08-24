224e5fe520a0290461b5f7853da204236afaf081
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-23 23:18:54 -0400

    test

--- chatnew.php
+++ chatnew.php
@@ -81,7 +81,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/chat.js?v17"></script>
+    <script src="assets/scripts/chat.js?v18"></script>
 </body>
 
 </html>
