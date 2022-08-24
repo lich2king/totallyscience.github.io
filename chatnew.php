df3bc5cd37f7fb5215af4ab18d0b3f2903bf68f6
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-23 23:21:52 -0400

    testub

--- chatnew.php
+++ chatnew.php
@@ -81,7 +81,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/chat.js?v19"></script>
+    <script src="assets/scripts/chat.js?v20"></script>
 </body>
 
 </html>
