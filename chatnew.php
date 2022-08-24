9c33eaf6bb76558b168f5936f7de593d7c3f0d65
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-23 23:47:38 -0400

    change cursor to point on leave btn

--- chatnew.php
+++ chatnew.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v12'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v13'>
 </head>
 
 <body>
