479be2cbe3a11b81bcc1fad32c19993d11e3f2ff
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-23 23:22:33 -0400

    tes

--- chatnew.php
+++ chatnew.php
@@ -81,7 +81,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/chat.js?v20"></script>
+    <script src="assets/scripts/chat.js?v21"></script>
 </body>
 
 </html>
