7ce4114191080378d3a660029f6af30119334e0e
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-08-24 22:23:57 -0400

    remove border on leave btn

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/chat.css?v14'>
+    <link rel='stylesheet' href='assets/styles/chat.css?v15'>
 </head>
 
 <body>
