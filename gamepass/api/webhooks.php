e3bbc1fc5d39373321501e7e3925a32a8b279be6
HackedZach <zay0106@icloud.com>
2022-08-29 12:11:38 -0400

    wer

--- gamepass/api/webhooks.php
+++ gamepass/api/webhooks.php
@@ -33,6 +33,20 @@ switch ($event->type) {
     $subscription = $event->data->object; // contains a \Stripe\Subscription
     // Then define and call a method to handle the subscription being created.
     // handleSubscriptionCreated($subscription);
+    $from = "help@totallyscience.co";
+    $to = "zay0106@icloud.com";
+    $subject = "YESSIRSKY";
+    $code = rand(10000,99999);
+    $message = "Your confirmation code is " . $code;
+    $headers = "From:" . $from;
+
+
+    if (mail($to, $subject, $message, $headers)) {
+      // email send client should show confirmation box
+      echo "success";
+    } else {
+      echo "The email message was not sent.";
+    }
     break;
   case 'customer.subscription.deleted':
     $subscription = $event->data->object; // contains a \Stripe\Subscription
