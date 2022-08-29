b0d9391db593d942836014a3f0a885e216569835
HackedZach <zay0106@icloud.com>
2022-08-29 11:29:12 -0400

    ewf

--- assets/scripts/checkout.js
+++ assets/scripts/checkout.js
@@ -22,7 +22,26 @@ async function initialize() {
     }).then((r) => r.json());
 
     const appearance = {
-        theme: 'stripe'
+        theme: 'night',
+        variables: {
+            fontFamily: 'Sohne, system-ui, sans-serif',
+            fontWeightNormal: '500',
+            borderRadius: '8px',
+            colorBackground: '#0A2540',
+            colorPrimary: '#EFC078',
+            colorPrimaryText: '#1A1B25',
+            colorText: 'white',
+            colorTextSecondary: 'white',
+            colorTextPlaceholder: '#727F96',
+            colorIconTab: 'white',
+            colorLogo: 'dark'
+        },
+        rules: {
+            '.Input, .Block': {
+                backgroundColor: 'transparent',
+                border: '1.5px solid var(--colorPrimary)'
+            }
+        }
     };
 
     elements = stripe.elements({ clientSecret, appearance });
