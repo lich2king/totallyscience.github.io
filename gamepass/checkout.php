6264a29207fd0a14d88577e8ea78d77ea261efd9
HackedZach <zay0106@icloud.com>
2022-08-29 17:23:07 -0400

    wfe

--- gamepass/checkout.php
+++ gamepass/checkout.php
@@ -24,7 +24,7 @@
       </div>
       <form action="api/create-checkout-session.php" method="POST">
         <!-- Add a hidden field with the lookup_key of your Price -->
-        <input type="hidden" name="lookup_key" value="gpannual" />
+        <input type="hidden" name="lookup_key" value="gpmonthly" />
         <button id="checkout-and-portal-button" type="submit">Checkout</button>
       </form>
     </section>
