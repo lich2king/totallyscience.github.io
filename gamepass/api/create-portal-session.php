d1e249282ec0fbc16518efeae587cf4cd0e3094b
HackedZach <zay0106@icloud.com>
2022-08-29 15:55:59 -0400

    wef

--- gamepass/api/create-portal-session.php
+++ gamepass/api/create-portal-session.php
@@ -1,6 +1,6 @@
 <?php
-require_once('stripe-php-9.4.0/init.php');
 
+require_once('stripe-php-9.4.0/init.php');
 // This is your real test secret API key.
 \Stripe\Stripe::setApiKey('sk_test_51LbBGpBRp1VXv1XhXArK0hGPZrl9XdeuZR8pywqPFpQRpWt3Uy3WzTfH1aJEk062WyIxa207bWjaQhAlaHeP3JmV00XjD3Uorj');
 
@@ -10,20 +10,16 @@ $YOUR_DOMAIN = 'http://mathstudyplace.com/gamepass/success.php';
 
 try {
   $checkout_session = \Stripe\Checkout\Session::retrieve($_POST['session_id']);
-} catch (Error $e) {
-  //http_response_code(500);
-  //echo json_encode(['error' => $e->getMessage()]);
-}
-  /*$return_url = $YOUR_DOMAIN;
+  $return_url = $YOUR_DOMAIN;
 
   // Authenticate your user.
   $session = \Stripe\BillingPortal\Session::create([
     'customer' => $checkout_session->customer,
     'return_url' => $return_url,
   ]);
-  //header("HTTP/1.1 303 See Other");
-  //header("Location: " . $session->url);
+  header("HTTP/1.1 303 See Other");
+  header("Location: " . $session->url);
 } catch (Error $e) {
   http_response_code(500);
   echo json_encode(['error' => $e->getMessage()]);
-}*/
+}
