177b6af7f2cc30efe22581b963d9f3d6f3c7e88f
HackedZach <zay0106@icloud.com>
2022-08-29 15:52:34 -0400

    wef

--- gamepass/api/create-portal-session.php
+++ gamepass/api/create-portal-session.php
@@ -10,15 +10,16 @@ $YOUR_DOMAIN = 'http://mathstudyplace.com/gamepass/success.php';
 
 try {
   $checkout_session = \Stripe\Checkout\Session::retrieve($_POST['session_id']);
-  $return_url = $YOUR_DOMAIN;
+  
+  /*$return_url = $YOUR_DOMAIN;
 
   // Authenticate your user.
   $session = \Stripe\BillingPortal\Session::create([
     'customer' => $checkout_session->customer,
     'return_url' => $return_url,
-  ]);
-  header("HTTP/1.1 303 See Other");
-  header("Location: " . $session->url);
+  ]);*/
+  //header("HTTP/1.1 303 See Other");
+  //header("Location: " . $session->url);
 } catch (Error $e) {
   http_response_code(500);
   echo json_encode(['error' => $e->getMessage()]);
