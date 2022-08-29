e0febd20e15c9877eb8417f67fc4e13b5fcc1deb
HackedZach <zay0106@icloud.com>
2022-08-29 13:37:42 -0400

    drfgdjthg

--- gamepass/api/create-checkout-session.php
+++ gamepass/api/create-checkout-session.php
@@ -26,21 +26,6 @@ try {
     'cancel_url' => $YOUR_DOMAIN . '/cancel.php',
   ]);
 
-  $from = "help@totallyscience.co";
-  $to = "zay0106@icloud.com";
-  $subject = "YESSIRSKY";
-  $code = rand(10000,99999);
-  $message = "Your confirmation code is " . $checkout_session;
-  $headers = "From:" . $from;
-
-
-  if (mail($to, $subject, $message, $headers)) {
-      // email send client should show confirmation box
-      echo "success";
-  } else {
-      echo "The email message was not sent.";
-  }
-
   header("HTTP/1.1 303 See Other");
   header("Location: " . $checkout_session->url);
 } catch (Error $e) {
