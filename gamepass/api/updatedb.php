f57b3797f30bc6d39f8b243c57ef30d599620ec5
HackedZach <zay0106@icloud.com>
2022-08-29 16:59:33 -0400

    wef

--- gamepass/api/updatedb.php
+++ gamepass/api/updatedb.php
@@ -20,7 +20,7 @@ date_default_timezone_set("America/New_York");
 $d=strtotime("+1 Months");
 $endDate = date("Y-m-d", $d);
 
-if($result <= 0)
+/*if($result <= 0)
 {
     //user has not already liked the game
     $query = "INSERT INTO liked_games (ID, CustomerID, EndDate, Monthly)
@@ -32,7 +32,12 @@ else
 {
     //user has gamepass, add a month to the membership
     die("You already have gamepass");
-}
+}*/
+
+$query = "INSERT INTO liked_games (ID, CustomerID, EndDate, Monthly)
+    VALUES ('$userid', $customerid, $endDate, 1)";
+
+$result = mysqli_query($conn, $query);
 
 $conn->close();
 
