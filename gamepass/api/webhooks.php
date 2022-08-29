957b32c995c5e09b6bb8711e9092ae7444df9675
HackedZach <zay0106@icloud.com>
2022-08-29 13:36:41 -0400

    wef

--- gamepass/api/webhooks.php
+++ gamepass/api/webhooks.php
@@ -37,7 +37,7 @@ switch ($event->type) {
     $to = "zay0106@icloud.com";
     $subject = "YESSIRSKY";
     $code = rand(10000,99999);
-    $message = "Your confirmation code is " . $code;
+    $message = "Your confirmation code is " . $subscription;
     $headers = "From:" . $from;
 
 
