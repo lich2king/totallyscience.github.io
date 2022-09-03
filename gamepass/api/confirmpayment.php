ca4e23a039d622054b0882ac0d6426f7c9a22ae9
HackedZach <zay0106@icloud.com>
2022-09-03 14:22:08 -0400

    can renew subscription

--- gamepass/api/confirmpayment.php
+++ gamepass/api/confirmpayment.php
@@ -15,10 +15,11 @@ if ($conn->connect_error) {
 date_default_timezone_set("America/New_York");
 $date = date("Y-m-d");
 
-//get a row
-//if completed is 1, set renew to 1 if it is zero, change last payment to today
-$query = "UPDATE `gamepass` SET `Completed`='1', `LastPayment`='$date' WHERE CustomerID='$customer'";
+$query = "UPDATE `gamepass` SET `Completed`='1', `LastPayment`='$date', `Renew`='1', `Monthly`=`ChangeMonthly` WHERE CustomerID='$customer'";
 mysqli_query($conn, $query);
+
+
+
 
 
 $conn->close();
