a73102147d6efd9775795f6dd5d58ea1b2f6b396
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-09-03 18:50:17 -0400

    test

--- assets/php/sendVerification.php
+++ assets/php/sendVerification.php
@@ -11,20 +11,20 @@
         die("connection failed");
     }
 
-    $user = json_decode($_COOKIE['logintoken'], true)['username'];
+    $id = json_decode($_COOKIE['logintoken'], true)['id'];
     $from = "help@totallyscience.co";
     $subject = "Totally Science Confirmation Code";
     $code = rand(10000,99999);
     $message = "Your confirmation code is " . $code;
     $headers = "From:" . $from;
 
-    if ($userresult = $conn->query("SELECT * FROM accounts WHERE Username = '$user'"))
+    if ($userresult = $conn->query("SELECT * FROM accounts WHERE id = '$id'"))
     {
         $row = $userresult -> fetch_row();
     
         $to = $row[1];
 
-        $conn->query("UPDATE accounts SET code = '$code' WHERE Username = '$user'");
+        $conn->query("UPDATE accounts SET code = '$code' WHERE id = '$id'");
     
         if (mail($to, $subject, $message, $headers)) {
             echo "Success";
