6cb7d772524276429cde13613b0b33ff461df6ad
HackedZach <zay0106@icloud.com>
2022-09-03 13:44:27 -0400

    wef

--- gamepass/misc/lastpay.php
+++ gamepass/misc/lastpay.php
@@ -28,8 +28,9 @@ $monthly = ($monthly -> fetch_row())[0];
 date_default_timezone_set("America/New_York");
 if($monthly == 1)
 {
-    $d=strtotime("+1 Months");
-    $date = date(strtotime($result), $d);
+    $startDate = '2014-06-03'; // select date in Y-m-d format
+    $nMonths = 1; // choose how many months you want to move ahead
+    $final = endCycle($result, $nMonths); // output: 2014-07-02
     echo($date);
 }else{
     $d=strtotime("+1 Years");
@@ -37,6 +38,34 @@ if($monthly == 1)
     echo($result);
 }
 
+
+function add_months($months, DateTime $dateObject) 
+    {
+        $next = new DateTime($dateObject->format('Y-m-d'));
+        $next->modify('last day of +'.$months.' month');
+
+        if($dateObject->format('d') > $next->format('d')) {
+            return $dateObject->diff($next);
+        } else {
+            return new DateInterval('P'.$months.'M');
+        }
+    }
+
+function endCycle($d1, $months)
+    {
+        $date = new DateTime($d1);
+
+        // call second function to add the months
+        $newDate = $date->add(add_months($months, $date));
+
+        // goes back 1 day from date, remove if you want same day of month
+        $newDate->sub(new DateInterval('P1D')); 
+
+        //formats final date to Y-m-d form
+        $dateReturned = $newDate->format('Y-m-d'); 
+
+        return $dateReturned;
+    }
 
 
 $conn->close();
