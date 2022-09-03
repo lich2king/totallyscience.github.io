390c6e03cc71559bae2f0e613f8790776eb04d99
HackedZach <zay0106@icloud.com>
2022-09-03 12:39:49 -0400

    wef

--- assets/php/login.php
+++ assets/php/login.php
@@ -23,11 +23,11 @@
             //check if user has game pass
             $hasGamePass = 'false';
             
-            /*$query = "SELECT `ID` FROM accounts WHERE `Username` = '$user'";
+            $query = "SELECT `ID` FROM accounts WHERE `Username` = '$user'";
             $userid = mysqli_num_rows(mysqli_query($conn, $query));
             $userid = ($userid -> fetch_row())[0];
 
-            $query = "SELECT * FROM gamepass WHERE ID = '$userid'";
+            /*$query = "SELECT * FROM gamepass WHERE ID = '$userid'";
             $result = mysqli_num_rows(mysqli_query($conn, $query));
             if($result > 0)
             {
