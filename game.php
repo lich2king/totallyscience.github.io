2886d63ca285f54314b038d67929d9e39985fe52
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-09-03 17:23:54 -0400

    cache

--- game.php
+++ game.php
@@ -58,7 +58,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/game.js?v48"></script>
+    <script src="assets/scripts/game.js?v49"></script>
 </body>
 
 </html>
