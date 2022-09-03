532a4519f65ee5a58f59bf3e22f311711dfe2fdb
HackedZach <zay0106@icloud.com>
2022-09-03 17:05:10 -0400

    weg

--- profile.php
+++ profile.php
@@ -73,7 +73,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/profile.js?v37"></script>
+    <script src="assets/scripts/profile.js?v38"></script>
 </body>
 
 </html>
