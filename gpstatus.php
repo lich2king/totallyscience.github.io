6b433196281c91f05cb61dfd4119bd297628c49b
HackedZach <zay0106@icloud.com>
2022-09-03 10:46:43 -0400

    wef

--- gpstatus.php
+++ gpstatus.php
@@ -30,7 +30,7 @@
             <li><a href="https://actuallyhistory.com" target="_blank">MathPlace.com</a></li>
         </ul>
         <a class="unsubscribe" onclick="unsubscribe()">Unsubscribe</a>
-        <form action="api/unsubscribe.php" method="POST">
+        <form action="gamepass/api/unsubscribe.php" method="POST">
             <!-- Add a hidden field with the lookup_key of your Price -->
             <input type="hidden" name="lookup_key" value="gpmonthly" />
             <button id="checkout-and-portal-button" type="submit">Checkout</button>
