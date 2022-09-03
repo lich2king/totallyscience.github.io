f1d5cfef4e2bf1991f722b9dabb88b85e4cac27a
HackedZach <zay0106@icloud.com>
2022-09-03 13:23:49 -0400

    wef

---
+++ gamepass/misc/lastpay.php
@@ -0,0 +1,45 @@
+<?php
+// READY
+
+include '../../assets/php/config.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+  
+// Check connection
+if ($conn->connect_error) {
+  die("connection failed"); //. $conn->connect_error);
+}
+
+if (!isset($_COOKIE['logintoken'])) {
+    die("no cookie");
+}
+  
+$userid = json_decode($_COOKIE['logintoken'], true)['id'];
+
+$query = "SELECT `LastPayment` FROM gamepass WHERE ID = '$userid'";
+$result = mysqli_query($conn, $query);
+$result = ($result -> fetch_row())[0];
+
+$query = "SELECT `Monthly` FROM gamepass WHERE ID = '$userid'";
+$monthly = mysqli_query($conn, $query);
+$monthly = ($result -> fetch_row())[0];
+
+date_default_timezone_set("America/New_York");
+if($monthly == 1)
+{
+    $d=strtotime("+1 Months");
+    $date = date($result, $d);
+    echo($result);
+}
+else{
+    $d=strtotime("+1 Years");
+    $date = date($result, $d);
+    echo($result);
+}
+
+
+
+$conn->close();
+
+?>
