b58f84c862968c1c1daa77daaa85c874638e781f
HackedZach <zay0106@icloud.com>
2022-09-03 18:32:19 -0400

    wef

--- game.php
+++ game.php
@@ -58,7 +58,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/main.js"></script>
-    <script src="assets/scripts/game.js?v51"></script>
+    <script src="assets/scripts/game.js?v52"></script>
 </body>
 
 </html>
