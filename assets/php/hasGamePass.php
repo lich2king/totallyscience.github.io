e7f705edaa78ebaf3f302b3747bcb5a11e0e1bfe
HackedZach <zay0106@icloud.com>
2022-09-03 17:58:03 -0400

    wef

--- assets/php/hasGamePass.php
+++ assets/php/hasGamePass.php
@@ -11,6 +11,12 @@ if ($conn->connect_error) {
   die("connection failed"); //. $conn->connect_error);
 }
 
+if (!isset($_COOKIE['logintoken'])) {
+    die("no cookie");
+}
+  
+$userid = json_decode($_COOKIE['logintoken'], true)['id'];
+
 
 //first check if the user has a game pass account
 $query = "SELECT * FROM gamepass WHERE ID = '$userid'";
