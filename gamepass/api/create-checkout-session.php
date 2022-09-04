bf93f498e80dc9239c73b4b08f601f09c5e9cdf0
HackedZach <zay0106@icloud.com>
2022-09-04 15:35:07 -0400

    ewf

--- gamepass/api/create-checkout-session.php
+++ gamepass/api/create-checkout-session.php
@@ -3,7 +3,7 @@
 require_once('stripe-php-9.4.0/init.php');
 
 // This is your test secret API key.
-\Stripe\Stripe::setApiKey('sk_test_51LbBGpBRp1VXv1XhXArK0hGPZrl9XdeuZR8pywqPFpQRpWt3Uy3WzTfH1aJEk062WyIxa207bWjaQhAlaHeP3JmV00XjD3Uorj');
+\Stripe\Stripe::setApiKey('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');
 
 header('Content-Type: application/json');
 
