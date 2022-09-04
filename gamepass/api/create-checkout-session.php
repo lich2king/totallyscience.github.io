8814f0e0a23d42c32144c8862fe762534e441f7a
HackedZach <zay0106@icloud.com>
2022-09-04 17:44:20 -0400

    we

--- gamepass/api/create-checkout-session.php
+++ gamepass/api/create-checkout-session.php
@@ -10,15 +10,13 @@ header('Content-Type: application/json');
 $YOUR_DOMAIN = 'http://mathstudyplace.com';
 
 try {
-  $prices = \Stripe\Product::retrieve(
+  $prices = \Stripe\Prices::search([
     // retrieve lookup_key from form data POST body
     //'lookup_keys' => [$_POST['lookup_key']],
     //'id' => 'price_1LcA10BRp1VXv1XhahWkIPJx',
-    //'query' => 'active:\'true\' AND metadata[\'order_id\']:\'6735\'',
-    //'expand' => ['data.product']
-    'prod_MKpgCQ6aPnHyPr',
-    []
-  );
+    'query' => 'active:\'true\' AND metadata[\'order_id\']:\'6735\'',
+    'expand' => ['data']
+  ]);
 
 
   $customer = \Stripe\Customer::create();
