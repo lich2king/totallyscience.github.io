bf7c329cecaba1c8d2dbfe0c6c07f482a59fda14
HackedZach <zay0106@icloud.com>
2022-09-05 15:19:33 -0400

    ad next to game

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             showAds();
             //if domain is not totallyscience.co, make sure user has game pass
-            if (location.hostname != "totallyscience.co" && !(this.location.href.includes("gamepass")) && !(this.location.href.includes("signup")) && !(this.location.href.includes("login")) && !(this.location.href.includes("profile"))) {
+            if (location.hostname != "totallyscience.co" && location.hostname != "localhost" && !(this.location.href.includes("gamepass")) && !(this.location.href.includes("signup")) && !(this.location.href.includes("login")) && !(this.location.href.includes("profile"))) {
                 this.location.href = "gamepass.php?gpdomain=1"
             }
         }
