894d2c5adb74c4e49cea06e30dc8b6151a649632
HackedZach <zay0106@icloud.com>
2022-09-05 14:32:08 -0400

    wef

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -10,7 +10,7 @@
 <meta name="google-site-verification" content="-gcWvd6d4OTZeFW4HWaIIZTNKnnOfAx15pEw_XDAH2o" />
 
 <!--Google Adsense-->
-<script data-ad-client="ca-pub-3486863589051210" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!--<script data-ad-client="ca-pub-3486863589051210" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y14RZLW8H8"></script>
 <script>
