153c49dcfdd2547cf3e88e79df44de1e48898499
HackedZach <zay0106@icloud.com>
2022-09-05 14:56:43 -0400

    wef

--- class.php
+++ class.php
@@ -11,15 +11,7 @@
 
 <body>
     <?php include "assets/includes/navbar.php" ?>
-
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
-        crossorigin="anonymous"></script>
-    <!-- NextToGameFrame -->
-    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210" data-ad-slot="3780993293"
-        data-ad-format="auto" data-full-width-responsive="true"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    
     <div id="gameframe">
         <div class="game">
             <iframe id="iframe" src=""></iframe>
@@ -28,33 +20,22 @@
         <div class="options">
             <div class="left">
                 <div class="undertext">
-                    <button id="like" class="text-button button-effect-a"><img
-                            src="assets/images/icons/likeoutline.png"></button>
+                    <button id="like" class="text-button button-effect-a"><img src="assets/images/icons/likeoutline.png"></button>
                     <div id="likeCount">...</div>
                 </div>
                 <div class="undertext">
-                    <button onclick="OpenHighscore()" class="text-button"><img
-                            src="https://www.nicepng.com/png/full/37-375088_icons-health-trophy-trophy-icon-white-png.png"></button>
+                    <button onclick="OpenHighscore()" class="text-button"><img src="https://www.nicepng.com/png/full/37-375088_icons-health-trophy-trophy-icon-white-png.png"></button>
                     <div id="currentHighscore">...</div>
                 </div>
                 <!--<button id="save" class="game-button"><img src="assets/images/icons/saveicon.png"></button>
                 <button class="game-button"><img src="assets/images/icons/upload.png"></button>-->
             </div>
             <div class="right">
-                <button id="pin" class="game-button button-effect-a"><img
-                        src="assets/images/icons/pinoutline.png"></button>
+                <button id="pin" class="game-button button-effect-a"><img src="assets/images/icons/pinoutline.png"></button>
                 <button id="fullscreen" class="game-button"><img src="assets/images/icons/fullscreen.png"></button>
             </div>
         </div>
     </div>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
-        crossorigin="anonymous"></script>
-    <!-- NextToGameFrame -->
-    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210" data-ad-slot="3780993293"
-        data-ad-format="auto" data-full-width-responsive="true"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
 
     <div id="info">
         <div>
@@ -76,7 +57,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-
+    
     <script src="assets/scripts/class.js?v56"></script>
 </body>
 
