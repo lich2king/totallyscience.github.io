ce97be06d4f233c58f9470a79b13e4afa4f6c0c0
HackedZach <zay0106@icloud.com>
2022-09-06 21:34:22 -0400

    wef

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -65,4 +65,4 @@
 
     <div class="bottomFoot">© 2022 TotallyScience. A Zach Yadegari Production ™</div>
 </footer>
-<script src="assets/scripts/main.js?v22"></script>
+<script src="assets/scripts/main.js?v23"></script>
