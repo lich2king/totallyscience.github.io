99199a5cb55cc816b9bd6411248a132ad20ec527
HackedZach <zay0106@icloud.com>
2022-09-08 19:56:49 -0400

    ewf

--- class.php
+++ class.php
@@ -15,7 +15,7 @@
 
     <div id="gameframe">
         <div class="ad">
-            <!--<div class="adsrc">
+            <div class="adsrc">
                 <script async
                     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
                     crossorigin="anonymous"></script>
@@ -24,7 +24,7 @@
                 <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
-            </div>-->
+            </div>
             <iframe class="adframe" scrolling="no" style="display:none; width:10vw; height:100vh; border: none;"
                 src=""></iframe>
         </div>
@@ -68,6 +68,8 @@
                 (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
             </div>
+            <iframe class="adframe" scrolling="no" style="display:none; width:10vw; height:100vh; border: none;"
+                src=""></iframe>
         </div>
     </div>
     <iframe class="adframe" style="display:none; width:100vw; height:40vh; border: none;" src=""></iframe>
