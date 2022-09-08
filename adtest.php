9ba8a75ad5e623773fcd1e72fc329dea26065125
HackedZach <zay0106@icloud.com>
2022-09-07 21:12:23 -0400

    ewfg

---
+++ adtest.php
@@ -0,0 +1,33 @@
+<!-- // READY -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/game.css?v10'>
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+
+    <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+                crossorigin="anonymous"></script>
+            <!-- NextToGameFrame -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+                data-ad-slot="3780993293" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
+
+    <?php include "assets/includes/footer.php" ?>
+
+
+    <script src="assets/scripts/class.js?v60"></script>
+</body>
+
+</html>
