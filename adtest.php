ddf759c78cbfc9d2602da5c171e0e90f0efbf0e1
HackedZach <zay0106@icloud.com>
2022-09-07 21:42:32 -0400

    wef

--- adtest.php
+++ adtest.php
@@ -9,8 +9,8 @@
     <link rel='stylesheet' href='assets/styles/game.css?v10'>
     <style>
         .ad {
-            width: 100vw;
-            height: 100vh;
+            width: 100%;
+            height: 100%;
         }
     </style>
 </head>
