acaf4758d1034941df05106e5db38cc4a6db6a1d
HackedZach <zay0106@icloud.com>
2022-09-07 21:54:07 -0400

    wef

--- class.php
+++ class.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/game.css?v18'>
+    <link rel='stylesheet' href='assets/styles/game.css?v19'>
 </head>
 
 <body>
