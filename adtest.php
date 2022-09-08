96fbb6d4c9dcf4372d1ed5665f5d6c9162fae0ca
HackedZach <zay0106@icloud.com>
2022-09-07 21:14:47 -0400

    wef

--- adtest.php
+++ adtest.php
@@ -7,18 +7,21 @@
     <?php include "assets/includes/head.php" ?>
 
     <link rel='stylesheet' href='assets/styles/game.css?v10'>
+    
 </head>
 
 <body>
-    <script async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
-                crossorigin="anonymous"></script>
-            <!-- NextToGameFrame -->
-            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
-                data-ad-slot="3780993293" data-ad-format="auto" data-full-width-responsive="true"></ins>
-            <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+    <div class="ad">
+        <script async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+            crossorigin="anonymous"></script>
+        <!-- NextToGameFrame -->
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+            data-ad-slot="3780993293" data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 
 
     <script src="assets/scripts/main.js?v23"></script>
