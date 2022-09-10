99c1309f9e53e2ce9566e5c678c0286d8e29f4c6
HackedZach <zay0106@icloud.com>
2022-09-10 18:18:24 -0400

    ewf

--- settings.php
+++ settings.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/settings.css'>
+    <link rel='stylesheet' href='assets/styles/settings.css?v1'>
 </head>
 
 <body>
