9af26849b799ad3bb3090eff35b22004380ca39b
HackedZach <zay0106@icloud.com>
2022-09-10 17:05:04 -0400

    wef

--- leaderboard.php
+++ leaderboard.php
@@ -24,6 +24,18 @@
         <button class="button" onclick="window.open('submithighscore.php', '_self')"><span>Submit Highscore</span> <img style="padding-left:0.5vw" width="16vw" src="https://www.cooperlighting.com/s/vividtune/png/right-arrow.png"></button>
     </div>
 
+    <div class="adsrc" style="text-align:center;">
+        <script async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+            crossorigin="anonymous"></script>
+        <!-- Horizontal Ad -->
+        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+            data-ad-slot="5628466741" data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+
     <div id="highscores"></div>
 
     <?php include "assets/includes/footer.php" ?>
