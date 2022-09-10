cc1d1f1e5ea9c301fd026a9cf2d485700c82f984
HackedZach <zay0106@icloud.com>
2022-09-10 18:10:44 -0400

    ewf

--- leaderboard.php
+++ leaderboard.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/leaderboard.css?v21'>
+    <link rel='stylesheet' href='assets/styles/leaderboard.css?v22'>
 </head>
 
 <body>
