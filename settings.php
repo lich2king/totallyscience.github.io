5a52eeeb2be101ccbc18875a646a03579fd57e58
HackedZach <zay0106@icloud.com>
2022-09-10 18:16:19 -0400

    wef

--- settings.php
+++ settings.php
@@ -31,6 +31,18 @@
         <button class="button" id="save-button" onclick="submit()">Save</button>
     </div>
 
+    <div class="adsrc" style="text-align:center; margin-top:50px;">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+                crossorigin="anonymous"></script>
+            <!-- Horizontal Ad -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+                data-ad-slot="5628466741" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/settings.js?v2"></script>
