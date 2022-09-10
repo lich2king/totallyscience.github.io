d173599c4a3b6ea5d2fcfa9ae25b6f2b170782d6
HackedZach <zay0106@icloud.com>
2022-09-10 18:12:26 -0400

    wef

--- chat.php
+++ chat.php
@@ -19,71 +19,177 @@
         <br>
         <button class="button">Join Public Chatroom</button>
         <p id="errorText" class="error"></p>
-        <script async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
-            crossorigin="anonymous"></script>
-        <!-- Horizontal Ad -->
-        <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
-            data-ad-slot="5628466741" data-ad-format="auto" data-full-width-responsive="true"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+        <div class="adsrc" style="text-align:center; margin-top:20px; margin-bottom:20px;">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+                crossorigin="anonymous"></script>
+            <!-- Horizontal Ad -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+                data-ad-slot="5628466741" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     </div>
 
     <div id="chat">
         <ul id="messages">
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
-            <li><span class="span"></span><span2></span2></li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
+            <li><span class="span"></span>
+                <span2></span2>
+            </li>
         </ul>
         <input style="display: none;" id="messageinput" placeholder="Message..." onclick=""></input>
         <button style="display: none;" id="leavebtn" onclick="location.reload();">Leave</button>
