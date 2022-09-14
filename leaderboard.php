fe9eec80be80dd192904922ff59fd3e5215e1d62
HackedZach <zay0106@icloud.com>
2022-09-14 17:55:20 -0400

    ewf

--- leaderboard.php
+++ leaderboard.php
@@ -20,13 +20,14 @@
 
     <h1 id="nohighscore" style="display:none"></h1>
 
-    <div style="text-align:center; margin-top: 20px; margin-bottom: 50px; z-index: 3;">
-        <button class="button" onclick="window.open('submithighscore.php', '_self')"><span>Submit Highscore</span> <img
-                style="padding-left:0.5vw" width="16vw"
-                src="https://www.cooperlighting.com/s/vividtune/png/right-arrow.png"></button>
-    </div>
+    
 
     <div id="highscores">
+        <div style="text-align:center; margin-top: 20px; margin-bottom: 50px; z-index: 3;">
+            <button class="button" onclick="window.open('submithighscore.php', '_self')"><span>Submit Highscore</span> <img
+                style="padding-left:0.5vw" width="16vw"
+                src="https://www.cooperlighting.com/s/vividtune/png/right-arrow.png"></button>
+        </div>
         <div id="verticalAds">
             <div class="contain-left">
                 <div class="left">
