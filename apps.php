88e0af226745a88fe50616d2a353538eb50f0cfc
HackedZach <zay0106@icloud.com>
2022-09-14 22:07:23 -0400

    wef

--- apps.php
+++ apps.php
@@ -12,6 +12,17 @@
 <body>
     <?php include "assets/includes/navbar.php" ?>
 
+    <div class="adsrc" style="text-align:center; margin-top:50px;">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3486863589051210"
+                crossorigin="anonymous"></script>
+            <!-- Horizontal Ad -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
+                data-ad-slot="5628466741" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     <div id="apps"></div>
 
     <?php include "assets/includes/footer.php" ?>
