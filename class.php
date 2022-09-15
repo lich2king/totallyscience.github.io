0ec62a8f7c75d4c41afe515727994859d7bd4fa6
Ilan <53833154+IlanLuci@users.noreply.github.com>
2022-09-15 17:06:50 -0400

    cache

--- class.php
+++ class.php
@@ -97,7 +97,7 @@
     <?php include "assets/includes/footer.php" ?>
 
 
-    <script src="assets/scripts/class.js?v65"></script>
+    <script src="assets/scripts/class.js?v66"></script>
 </body>
 
 </html>
