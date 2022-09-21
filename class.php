63857929a71f8134f88ed53f3bff94dd1b95c691
TotallyScience <zay0106@icloud.com>
2022-09-21 09:25:23 -0400

    ewf

--- class.php
+++ class.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/game.css?v20'>
+    <link rel='stylesheet' href='assets/styles/class.css?v20'>
 </head>
 
 <body>
