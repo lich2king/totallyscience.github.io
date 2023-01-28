90bb73572c4ff249d633f6a21c702ce07e21cb62
HackedZach <zay0106@icloud.com>
2023-01-28 18:11:35 -0500

    wef

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -588,7 +588,7 @@ function setRewardDayBar() {
 
                 console.log(rewardDay);
                 console.log(day);
-                let w = (100 / 7) * (day + 1);
+                let w = (100 / 7) * (1 + 1);
 
                 document.getElementById('rewardDayBar').style = `width: ${w}%`;
             });
