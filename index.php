e8541e1f7056ce1e277eb1c64e9ed54d51a96e99
HackedZach <zay0106@icloud.com>
2023-01-29 13:19:56 -0500

    wef

--- index.php
+++ index.php
@@ -341,7 +341,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/index.js?v122"></script>
+    <script src="assets/scripts/index.js?v123"></script>
 </body>
 
 </html>
