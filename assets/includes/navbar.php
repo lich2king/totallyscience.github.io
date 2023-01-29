6eb770ff58b3fa716dca1e03238b111c82807498
HackedZach <zay0106@icloud.com>
2023-01-29 13:16:06 -0500

    shop grayed out on nav bar

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -6,6 +6,7 @@
         <div class="nav-button" id="leaderboardnav"><a href="leaderboard">&#127942; Leaderboard</a></div>
         <div class="nav-button" id="chatnav"><a href="chat">&#128172; Chat</a></div>
         <div class="nav-button" id="appsnav"><a href="apps">&#127922; Apps</a></div>
+        <div class="nav-button" tooltip="Coming soon!" flow="down" id="shopnav"><a>&#128722; Shop</a></div>
     </div>
 
     <div id="middlebar">
