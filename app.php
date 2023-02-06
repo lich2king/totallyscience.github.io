1869a3acd7c6df2984367527a835548abffbb86b
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-02-06 18:14:03 -0500

    proxy testing

--- app.php
+++ app.php
@@ -13,9 +13,7 @@
 
     <div style="display: flex;">
         <div style="margin-top: 55px; overflow: hidden; height: 91vh; width: 85vw;">
-            <iframe class="iframeloader" id="app_frame"
-                style="border: none; position: relative; z-index: 2; overflow: hidden;" width=100% height=100%
-                src=""></iframe>
+            <embed class="iframeloader" id="app_frame" style="border: none; position: relative; z-index: 2; overflow: hidden;" width=100% height=100% src=""></embed>
         </div>
         <div style="margin-top: 55px; overflow: hidden; height: 91vh; width: 15vw;">
             <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3486863589051210"
@@ -27,7 +25,7 @@
     </div>
 
     <script src="assets/scripts/main.js?v27"></script>
-    <script src="assets/scripts/app.js?5"></script>
+    <script src="assets/scripts/app.js?6"></script>
 </body>
 
 </html>
