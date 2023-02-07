3b02519be91c0b2c4bc72583ea98a9c1c256e545
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-02-07 17:53:16 +0000

    cache

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,35 +1,3 @@
-<!-- // READY -->
-
-<!--<div id="gamePassAd" style="display:none;">
-    <div>
-        <img src="assets/images/icons/lock.png" />
-        <p>More Games</p>
-    </div>
-
-    <div>
-        <img
-            src="assets/images/icons/fullscreen.png" />
-        <p>Fullscreen</p>
-    </div>
-
-    <div>
-        <h1>GAME <span>PASS</span></h1>
-        <button onclick="location.href = 'gamepass'">Get It Now</button>
-    </div>
-
-    <div>
-        <img
-            src="assets/images/icons/block-ads.png" />
-        <p>No Ads</p>
-    </div>
-
-    <div>
-        <img
-            src="assets/images/icons/eye.png" />
-        <p>More Links</p>
-    </div>
-</div>-->
-
 <footer>
     <div class="footSections">
         <div class="footSection">
@@ -71,4 +39,4 @@
 
     <div class="bottomFoot">© 2022 TotallyScience. A Zach Yadegari Production ™</div>
 </footer>
-<script src="assets/scripts/main.js?v32"></script>
+<script src="assets/scripts/main.js?v33"></script>
