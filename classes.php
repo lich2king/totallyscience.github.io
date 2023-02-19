a8be3f984f713ad489bb89bebb1fc4bbc61c21ba
TotallyScience <zay0106@icloud.com>
2023-02-19 16:59:11 -0700

    didn't await `\_O_/'

--- classes.php
+++ classes.php
@@ -40,7 +40,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/classes.js?v85"></script>
+    <script src="assets/scripts/classes.js?v86"></script>
 </body>
 
 </html>
