e4beb0cf1a40c35b0ca560e56694f4a87ada801f
TotallyScience <zay0106@icloud.com>
2023-02-20 12:06:06 -0700

    wef

--- profile.php
+++ profile.php
@@ -66,7 +66,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/profile.js?v53"></script>
+    <script src="assets/scripts/profile.js?v54"></script>
 </body>
 
 </html>
