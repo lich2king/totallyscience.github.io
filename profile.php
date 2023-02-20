773d12d461eb88540f10cb7942a64d3ccbc4539e
TotallyScience <zay0106@icloud.com>
2023-02-20 12:26:11 -0700

    ewf

--- profile.php
+++ profile.php
@@ -66,7 +66,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/profile.js?v57"></script>
+    <script src="assets/scripts/profile.js?v58"></script>
 </body>
 
 </html>
