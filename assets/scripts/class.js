c79d4a80280b059079dbd985f3403e12f6010a37
TotallyScience <zay0106@icloud.com>
2023-02-20 12:58:20 -0700

    wef

--- assets/scripts/class.js
+++ assets/scripts/class.js
@@ -249,8 +249,6 @@ function suggestGames() {
         }
     }
 
-    document.getElementById('suggestedGames').innerHTML = '';
-
 
     let arrowContainer = '<div class="arrowsCon"><div class="arrowCon arrowLeftCon" id="arrowLeft" style="visibility: hidden;"><img class="arrow" src="/assets/images/left-arrow.png"></div><div class="arrowCon arrowRightCon" id="arrowRight" ><img class="arrow" src="/assets/images/right-arrow.png"></div></div>'
     let gamesDiv = document.getElementById('games');
--- class.php
+++ class.php
@@ -89,7 +89,7 @@
     <?php include "assets/includes/footer.php" ?>
 
 
-    <script src="assets/scripts/class.js?v76"></script>
+    <script src="assets/scripts/class.js?v77"></script>
 </body>
 
 </html>
