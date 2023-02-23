ab031e7321952c2af28bd7674171a577a8499d7d
TotallyScience <zay0106@icloud.com>
2023-02-23 09:43:34 -0700

    fre

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='./assets/styles/account.css?v2'>
+    <link rel='stylesheet' href='./assets/styles/account.css?v3'>
 </head>
 
 <body>
