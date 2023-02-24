14769d8e5a0283cd543a05fe9990975a287cc1d1
TotallyScience <zay0106@icloud.com>
2023-02-23 18:10:53 -0700

    ewf

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -43,40 +43,14 @@ gtag('js', new Date());
 gtag('config', 'G-Y14RZLW8H8');
 </script>
 
-<!--<script type="text/javascript">
-(function(document, window){
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "https://api.pushnami.com/scripts/v1/pushnami-adv/62c84c2325d8fd0013162b2a";
-    script.onload = function() {
-        Pushnami
-            .update()
-            .prompt();
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-})(document, window);
-</script>-->
 
 <script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
     data-ad-client="ca-pub-3486863589051210" async="true"></script>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<!-- <script src="https://browser.sentry-cdn.com/7.12.1/bundle.tracing.min.js"
-    integrity="sha384-+5SuXMl3TMl8hMxuOnzV/WA9uCrxgqfg5AeYcRa3Xgt5+IicBBuHRUoYgHU741xO" crossorigin="anonymous">
-</script>
-<script>
-Sentry.init({
-    dsn: "https://ee9aa27951244138b295702d9a1d9443@o1410974.ingest.sentry.io/6749153",
-    integrations: [new Sentry.BrowserTracing()],
-
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-});
-</script> -->
 
 
-<link rel="stylesheet" href="assets/styles/main.css?v19">
+<link rel="stylesheet" href="assets/styles/main.css?v20">
 <link rel="icon" href="assets/images/logo.png">
 
 <title>Totally Science</title>
