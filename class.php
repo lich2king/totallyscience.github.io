d9067b4eabbd8450e9c10f5d85ce788df19b983a
TotallyScience <zay0106@icloud.com>
2023-02-24 22:14:45 -0700

    wef

--- class.php
+++ class.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/class.css?v39'>
+    <link rel='stylesheet' href='assets/styles/class.css?v40'>
 </head>
 
 <body>
