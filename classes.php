6597f4cbe5c8272ad02c33a104a0548f0bd6b88b
TotallyScience <zay0106@icloud.com>
2023-02-28 20:44:24 -0500

    yes

--- classes.php
+++ classes.php
@@ -19,6 +19,15 @@
             <input type="text" name="search" value="" autocomplete="off" id="searchBar" placeholder="Search">
         </div>
 
+        <div class="adsrc adhorz" style="text-align:center;">
+            <!-- Under the Game -->
+            <ins class="adsbygoogle" style="display:inline-block;width:800px;height:100px"
+                data-ad-client="ca-pub-3486863589051210" data-ad-slot="3824383684"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        
         <div id="filteredGames">
 
         </div>
