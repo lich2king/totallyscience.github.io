90333e37fdc60e7a275781ae465f39efd4655673
TotallyScience <zay0106@icloud.com>
2023-02-28 20:39:16 -0500

    category checks

--- classes.php
+++ classes.php
@@ -40,7 +40,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/classes.js?v98"></script>
+    <script src="assets/scripts/classes.js?v99"></script>
 </body>
 
 </html>
