8ed5e6a94238b40fdf740ea172fd021e9c998a03
HackedZach <zay0106@icloud.com>
2023-02-28 19:21:55 -0500

    wef

--- class.php
+++ class.php
@@ -6,7 +6,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/class.css?v58'>
+    <link rel='stylesheet' href='assets/styles/class.css?v59'>
 </head>
 
 <body>
