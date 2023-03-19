6c9787809e5afc730418d1665408d6a7c243e346
HackedZach <zay0106@icloud.com>
2023-03-19 17:02:31 -0400

    start shop

---
+++ shop.php
@@ -0,0 +1,97 @@
+<!-- // READY -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <link rel='stylesheet' href='assets/styles/shop.css?v0'>
+
+    <title>Shop - Totally Science - Free Online Unblocked Games</title>
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+
+    <div id="dispenserCon">
+        <img src="\assets\minis\Dispenser.gif">
+        <button class="button">1000 pts</button>
+    </div>
+
+    <div id="minis">
+        <h1>
+            Common Minis
+        </h1>
+        <div class="miniCon">
+            <img src="\assets\minis\!Avatar31.gif">
+            <img src="\assets\minis\!Avatar32.gif">
+            <img src="\assets\minis\!Avatar33.gif">
+            <img src="\assets\minis\!Avatar34.gif">
+            <img src="\assets\minis\!Avatar35.gif">
+            <img src="\assets\minis\!Avatar37.gif">
+            <img src="\assets\minis\!Avatar38.gif">
+            <img src="\assets\minis\!Avatar39.gif">
+            <img src="\assets\minis\!Avatar40.gif">
+            <img src="\assets\minis\!Avatar41.gif">
+            <img src="\assets\minis\!Avatar42.gif">
+            <img src="\assets\minis\!Avatar43.gif">
+            <img src="\assets\minis\!Avatar44.gif">
+            <img src="\assets\minis\!Avatar46.gif">
+            <img src="\assets\minis\!Avatar47.gif">
+            <img src="\assets\minis\!Avatar48.gif">
+            <img src="\assets\minis\!Avatar49.gif">
+            <img src="\assets\minis\!Avatar50.gif">
+        </div>
+        <h1>
+            Rare Minis
+        </h1>
+        <div class="miniCon">
+            <img src="\assets\minis\!Avatar16.gif">
+            <img src="\assets\minis\!Avatar17.gif">
+            <img src="\assets\minis\!Avatar19.gif">
+            <img src="\assets\minis\!Avatar20.gif">
+            <img src="\assets\minis\!Avatar21.gif">
+            <img src="\assets\minis\!Avatar22.gif">
+            <img src="\assets\minis\!Avatar23.gif">
+            <img src="\assets\minis\!Avatar24.gif">
+            <img src="\assets\minis\!Avatar25.gif">
+            <img src="\assets\minis\!Avatar26.gif">
+            <img src="\assets\minis\!Avatar29.gif">
+            <img src="\assets\minis\!Avatar30.gif">
+
+        </div>
+        <h1>
+            Epic Minis
+        </h1>
+        <div class="miniCon">
+            <img src="\assets\minis\!Avatar06.gif">
+            <img src="\assets\minis\!Avatar07.gif">
+            <img src="\assets\minis\!Avatar08.gif">
+            <img src="\assets\minis\!Avatar10.gif">
+            <img src="\assets\minis\!Avatar11.gif">
+            <img src="\assets\minis\!Avatar12.gif">
+            <img src="\assets\minis\!Avatar13.gif">
+            <img src="\assets\minis\!Avatar14.gif">
+            <img src="\assets\minis\!Avatar15.gif">
+        </div>
+        <h1>
+            Legendary Minis
+        </h1>
+        <div class="miniCon">
+            <img src="\assets\minis\!Avatar01.gif">
+            <img src="\assets\minis\!Avatar02.gif">
+            <img src="\assets\minis\!Avatar03.gif">
+            <img src="\assets\minis\!Avatar04.gif">
+            <img src="\assets\minis\!Avatar05.gif">
+        </div>
+    </div>
+
+
+    <?php #include "assets/includes/footer.php" ?>
+
+    <script src="assets/scripts/shop.js?v12"></script>
+</body>
+
+</html>
