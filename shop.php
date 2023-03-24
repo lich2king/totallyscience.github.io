6379b80ccd9c896a1b79e35d3f70cfa3f205934d
TotallyScience <zay0106@icloud.com>
2023-03-24 14:31:29 -0400

    erg

--- shop.php
+++ shop.php
@@ -94,7 +94,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/shop.js?v16"></script>
+    <script src="assets/scripts/shop.js?v17"></script>
 </body>
 
 </html>
