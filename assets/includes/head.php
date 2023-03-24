1a914f350707464bc2c6f525f9637c8cae7641fd
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-03-24 16:55:05 -0400

    attempt to disable ads on chat page

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -1,5 +1,7 @@
 <!-- // READY -->
 
+<title>Totally Science - Free Online Unblocked Games</title>
+
 <meta property="og:title" content="Totally Science - Free Online Unblocked Games">
 <meta property="og:site_name" content="Totally Science">
 <meta property="og:url" content="https://totallyscience.co">
@@ -17,12 +19,10 @@
 <meta name="DC.format" content="text/html" scheme="DCTERMS.DCMIType">
 <meta name="DC.publisher" content="Totally Science" scheme="DCTERMS.DCMIType">
 <meta name="DC.description" content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?" scheme="DCTERMS.DCMIType">
-<meta name="DC.subject" content="totally science, games, gaming, online games, unblocked games"
-    scheme="DCTERMS.DCMIType">
+<meta name="DC.subject" content="totally science, games, gaming, online games, unblocked games" scheme="DCTERMS.DCMIType">
 <meta name="DC.creator" content="Zach Yadegari">
 <meta name="subject" content="totally science, games, gaming, online games, unblocked games">
-<meta name="keywords"
-    content="totally science, games, gaming, online games, unblocked games, discord unblocked, minecraft online, roblox unblocked">
+<meta name="keywords" content="totally science, games, gaming, online games, unblocked games, discord unblocked, minecraft online, roblox unblocked">
 <meta name="twitter:description" content="Play free online games at Totally Science - your one-stop destination for free online games! io games, puzzle games, Flash games, shooting games and more. And why &quot;Totally Science&quot;? It's our secret for bypassing school blockers. Sit back, relax, and enjoy learning...science?">
 <meta name="twitter:url" content="https://totallyscience.co">
 <meta name="twitter:image" content="https://totallyscience.co/assets/images/totally-science-logo-transparent-scaled.png">
@@ -33,33 +33,30 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y14RZLW8H8"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-gtag('config', 'G-Y14RZLW8H8');
+    gtag('config', 'G-Y14RZLW8H8');
 </script>
 
+<!-- google adsense is not currently in use -->
+<!-- <script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" data-ad-client="ca-pub-3486863589051210" async="true"></script> -->
 
-<!-- <script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-    data-ad-client="ca-pub-3486863589051210" async="true"></script> -->
-
-    <!-- WG Universal adCode -->
+<!-- WG Universal adCode -->
 <script type="text/javascript" async>
-    !function(e,t){a=e.createElement("script"),m=e.getElementsByTagName("script")[0],a.async=1,a.src=t,m.parentNode.insertBefore(a,m)}(document,"https://universal.wgplayer.com/tag/?lh="+window.location.hostname+"&wp="+window.location.pathname+"&ws="+window.location.search);
+    if (location.pathname != '/chat') {
+        !function(e,t){a=e.createElement("script"),m=e.getElementsByTagName("script")[0],a.async=1,a.src=t,m.parentNode.insertBefore(a,m)}(document,"https://universal.wgplayer.com/tag/?lh="+window.location.hostname+"&wp="+window.location.pathname+"&ws="+window.location.search);
+    }
 </script>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-
 <!-- Lazy loading -->
 <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=IntersectionObserver"></script>
 
-
 <link rel="stylesheet" href="assets/styles/main.css?v21">
 <link rel="icon" href="assets/images/totally-science-logo-transparent-scaled.png">
-
-<title>Totally Science - Free Online Unblocked Games</title>
