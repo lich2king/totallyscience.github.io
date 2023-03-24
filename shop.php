0af5b6c51427dfbb1b863e31afcebd5c4b2cc652
TotallyScience <zay0106@icloud.com>
2023-03-24 14:39:58 -0400

    wef

--- shop.php
+++ shop.php
@@ -94,7 +94,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/shop.js?v21"></script>
+    <script src="assets/scripts/shop.js?v22"></script>
 </body>
 
 </html>
