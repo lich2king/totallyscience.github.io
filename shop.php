60f3eb48f5cd4ca803ef0784182648a44ef07a83
HackedZach <zay0106@icloud.com>
2023-03-25 20:56:23 -0400

    color fix

--- shop.php
+++ shop.php
@@ -94,7 +94,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/shop.js?v33"></script>
+    <script src="assets/scripts/shop.js?v34"></script>
 </body>
 
 </html>
