75be57f41557e949a6fe7245730e710ddd801291
HackedZach <zay0106@icloud.com>
2023-03-29 18:48:21 -0400

    wef

--- shop.php
+++ shop.php
@@ -94,7 +94,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/shop.js?v46"></script>
+    <script src="assets/scripts/shop.js?v47"></script>
 </body>
 
 </html>
