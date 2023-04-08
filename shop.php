e92927e7d9b1182fe8b85eb56e54bcebe1001c74
HackedZach <zay0106@icloud.com>
2023-04-08 13:38:50 -0400

    wef

--- shop.php
+++ shop.php
@@ -280,17 +280,20 @@
         const dispenseButton = document.getElementById('dispenseButton');
 
         <<
-        << << < HEAD
+        <<
+        << < HEAD
         if (vid.paused && token) {
             let currentVal = document.getElementById('pointsDisplay').innerText;
 
             counter('pointsDisplay', parseInt(currentVal), parseInt(currentVal - 1000), 2000);
 
             dispenseButton.innerHTML = 'Dispensing...'; ===
-            === =
+            ===
+            =
             if (vid.paused && token) {
                 dispenseButton.innerHTML = 'Dispensing...'; >>>
-                >>> > 9911 d09b385fb39724890bd4c973224b699c78bb
+                >>>
+                > 9911 d09b385fb39724890bd4c973224b699c78bb
 
                 playVid();
             } else if (!token) {
