a7c10f1e05c7621e68ac4612f8398ef5de7271f8
TotallyScience <zay0106@icloud.com>
2023-04-15 17:15:05 -0400

    wef

--- class.php
+++ class.php
@@ -89,7 +89,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/class.js?v127"></script>
+    <script src="assets/scripts/class.js?v128"></script>
 </body>
 
 </html>
