62499bea78dcbdef1632f06dac52da582fe840e3
TotallyScience <zay0106@icloud.com>
2023-04-21 14:09:19 -0400

    wef

--- index.php
+++ index.php
@@ -321,7 +321,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/index.js?v63"></script>
+    <script src="assets/scripts/index.js?v64"></script>
 </body>
 
 </html>
