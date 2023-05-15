5eb8af926a690db9c2b041f088e70bf72d4b54db
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-15 09:42:11 -0400

    test

--- deploy_backend.php
+++ deploy_backend.php
@@ -6,41 +6,5 @@
      *
      */
 
-    // array of commands
-    $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
-        'npm i'
-    );
-
-    // exec commands
-    $output = '';
-
-    foreach($commands AS $command){
-        $tmp = exec('cd /home/ts-backend/ && ' . $command);
-        
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
-    }
+     echo shell_exec('sh /home/ts-backend/pull.sh');
 ?>
-
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script</p>
-    <?php echo $output; ?>
-    </div>
-</div>
-</body>
-</html>
