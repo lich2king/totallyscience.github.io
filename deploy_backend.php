6b9ed3173812a98e1884c9785da1dbf0eae53326
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-15 09:30:16 -0400

    testing deployment

--- deploy_backend.php
+++ deploy_backend.php
@@ -20,8 +20,9 @@
 
     // exec commands
     $output = '';
+
     foreach($commands AS $command){
-        $tmp = shell_exec('cd ~/ts-backend/ && ' . $command);
+        $tmp = shell_exec('cd /home/ts-backend/ && ' . $command);
         
         $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
         $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
