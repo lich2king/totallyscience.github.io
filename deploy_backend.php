e98d71e3f21011c1a9dc64f54631645075d51704
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-15 09:36:39 -0400

    test

--- deploy_backend.php
+++ deploy_backend.php
@@ -24,7 +24,7 @@
     chdir('/home/ts-backend/');
 
     foreach($commands AS $command){
-        $tmp = shell_exec($command);
+        $tmp = exec($command);
         
         $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
         $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
