c5108fd7057fbb68418f02fccd6691065a282054
HackedZach <zay0106@icloud.com>
2023-05-15 20:51:40 -0400

    font size

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/index.css?v41'>
+    <link rel='stylesheet' href='assets/styles/index.css?v42'>
     <script src="/assets/scripts/fontawesome.js"></script>
 </head>
 
