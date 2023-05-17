d288174a37f87c13891f793a61ea3fa0f7a3dd7f
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-17 17:38:25 -0400

    add dynamic to subdirectory

---
+++ dynamic/dynamic/dynamic.config.js
@@ -0,0 +1,28 @@
+self.__dynamic$config = {
+  prefix: '/dyn/',
+  encoding: 'plain',
+  mode: 'production', // development: zero caching, no minification, production: speed-oriented
+  rewrite: {
+    css: 'regex',
+    js: 'acorn',
+    html: 'htmlparser2',
+  },
+  bare: {
+    version: 2,
+    path: '/bare/',
+  },
+  tab: {
+    title: 'procy',
+    icon: 'https://www.google.com/favicon.ico',
+    ua: 'Mozilla/5.0 (X11; CrOS x86_64 14388.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.107 Safari/537.36',
+  },
+  assets: {
+    prefix: '/dynamic/',
+    files: {
+      handler: 'dynamic.handler.js',
+      client: 'dynamic.client.js',
+      worker: 'dynamic.worker.js',
+      config: 'dynamic.config.js',
+    }
+  }
+};
