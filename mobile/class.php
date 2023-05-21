ec27b5369777793e81fb7d61579f8db0c78932c7
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-05-20 22:23:25 -0400

    mobile improvements
    - improve exclamation sizing on game page
    - move styles and JS into php

--- mobile/class.php
+++ mobile/class.php
@@ -1,18 +1,43 @@
-<!-- // READY -->
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php include "../assets/includes/head.php" ?>
-    <link rel='stylesheet' href='assets/styles/game.css?v11'>
+
+    <style>
+        #gameContainer {
+            margin-top: 20px;
+            width: 100vw;
+            height: 85vh;
+            margin-bottom: 5%;
+        }
+
+        #gameFrameDiv {
+            width: 100%;
+            height: 100%;
+            text-align: center;
+        }
+
+        #gameFrameDiv iframe {
+            width: 75%;
+            height: 100%;
+            border: 4px solid var(--accent-color);
+            border-radius: 20px;
+            margin-bottom: 0%;
+            resize: vertical;
+        }
+
+        #alertIcon {
+            height: 6vh;
+        }
+     </style>
 </head>
 
 <body>
     <section id="fixedElementsSection">
         <div id="fixedElements">
-            <button onclick="window.open('index', '_self')" id="backarrow"><img id="backarrowLogo" src="../assets/images/backarrow-light.svg"></button>
-            <button onclick="openSchoolSite()" id="alert"><img id="alertIcon" src="../assets/images/exclamation-light.svg"></button>
+            <button onclick="window.open('index.php', '_self')" id="backarrow"><img id="backarrowLogo" src="../assets/images/backarrow-light.svg"></button>
+            <button onclick="openSchoolSite()" id="alert"><img id="alertIcon" src="assets/exclamation.svg"></button>
         </div>
     </section>
 
@@ -22,10 +47,27 @@
         </div>
     </section>
 
-        
-
     <script src="assets/scripts/main.js?v65"></script>
-    <script src="assets/scripts/load_game.js?v64"></script>
+    <script>
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const gameName = urlParams.get('class');
+
+        fetch(`../assets/games.json?date=${new Date().getTime()}`).then((response) => response.json()).then((games) => {
+                const gameData = games[gameName];
+
+                if (gameData == null) window.location.href = 'index.php';
+
+                // update game view statistics
+                fetch(`/stats/games/view`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: { gameName: gameName } });
+
+                var theIframeUrl = gameData.iframe_url;
+                if (theIframeUrl[0] == '.' || theIframeUrl[0] == '/' || theIframeUrl[0] == 'a') {
+                    theIframeUrl = `../${theIframeUrl}`;
+                }
+                $('game-iframe').src = theIframeUrl;
+        });
+    </script>
 </body>
 
 </html>
