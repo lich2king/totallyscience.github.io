ba999327db74297d82e9e4065f1e22fe51df728b
TotallyScience <zay0106@icloud.com>
2023-05-21 12:20:23 -0400

    ewf

--- mobile/index.php
+++ mobile/index.php
@@ -5,7 +5,7 @@
 
 <head>
     <?php include "../assets/includes/head.php" ?>
-    <link rel='stylesheet' href='assets/styles/main.css?v26'>
+    <link rel='stylesheet' href='assets/styles/main.css?v27'>
 </head>
 
 <body>
