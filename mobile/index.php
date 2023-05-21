011150ec1806c445c2f10ba8d7635c6690fb95af
HackedZach <zay0106@icloud.com>
2023-05-20 20:09:36 -0400

    wef

--- mobile/index.php
+++ mobile/index.php
@@ -5,7 +5,7 @@
 
 <head>
     <?php include "../assets/includes/head.php" ?>
-    <link rel='stylesheet' href='assets/styles/main.css?v20'>
+    <link rel='stylesheet' href='assets/styles/main.css?v21'>
 </head>
 
 <body>
