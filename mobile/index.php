b21169ce2489b5268e08be5a6942868bd5c62d4e
HackedZach <zay0106@icloud.com>
2023-05-21 18:51:33 -0400

    wqefd

--- mobile/index.php
+++ mobile/index.php
@@ -5,7 +5,7 @@
 
 <head>
     <?php include "../assets/includes/head.php" ?>
-    <link rel='stylesheet' href='assets/styles/main.css?v40'>
+    <link rel='stylesheet' href='assets/styles/main.css?v40v2'>
 </head>
 
 <body>
