e4140158ad44b655f8e16f798bc1f3910e2a1ec3
TotallyScience <zay0106@icloud.com>
2023-05-22 21:16:50 -0400

    wef

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <?php include "assets/includes/head.php" ?>
 
     <link rel='stylesheet' href='assets/styles/index.css?v43'>
-    <script src="/assets/scripts/fontawesome.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 </head>
 
 <body>
