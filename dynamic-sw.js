b4cc5d00b6fd207cc39b1726e406cedcc8d5ac55
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-06-02 18:34:21 -0400

    Revert "Merge pull request #5 from TotallyScience/dynamic"
    This reverts commit 32142f25b5f01abe7e168d487f8860801dfa03f5, reversing
    changes made to a886421a42b1bb26b3ec830efff44a5ff484bcb2.

--- dynamic-sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('./assets/scripts/dynamic/dynamic.worker.js');
-
-const sw = new DynamicSW();
-
-self.addEventListener('fetch', event =>
-    event.respondWith(
-        sw.fetch(event)
-    )
-);
