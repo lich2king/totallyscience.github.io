2ac61071c44b5b2513e9ff33490801260b745af7
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-08-28 23:38:54 -0400

    remove font awesome requirements for performance

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <?php include "assets/includes/head.php" ?>
 
     <link rel='stylesheet' href='assets/styles/index.css?v43'>
-    <script src="/assets/scripts/fontawesome0.js"></script>
 </head>
 
 <body>
@@ -39,7 +38,7 @@
                 </div>
 
                 <button onclick="window.open('class.php?class=Monkey-Mart', '_self')" class="featureButton button">Play
-                    Now <i class="fa-solid fa-angle-right fa-lg" style="color: #ffffff;"></i></button>
+                    Now <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
             </div>
 
             <div class="featureSlot fade"
@@ -51,8 +50,7 @@
                 </div>
 
                 <button onclick="window.open('class.php?class=Bob-the-Robber-2', '_self')"
-                    class="featureButton button">Play Now <i class="fa-solid fa-angle-right fa-lg"
-                        style="color: #ffffff;"></i></button>
+                    class="featureButton button">Play Now <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
             </div>
 
             <div class="featureSlot fade"
@@ -65,7 +63,7 @@
 
                 <button onclick="window.open('class.php?class=Moto-X3M-Pool-Party', '_self')"
                     class="featureButton button">Play
-                    Now <i class="fa-solid fa-angle-right fa-lg" style="color: #ffffff;"></i></button>
+                    Now <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
             </div>
 
             <div class="featureSlot fade"
@@ -77,7 +75,7 @@
                 </div>
 
                 <button onclick="window.open('class.php?class=Vex-7', '_self')" class="featureButton button">Play
-                    Now <i class="fa-solid fa-angle-right fa-lg" style="color: #ffffff;"></i></button>
+                    Now <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg></button>
             </div>
         </div>
 
