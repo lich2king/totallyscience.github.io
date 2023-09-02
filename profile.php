b4124b428a178460d15116357eedbd944e4a782a
HackedZach <zay0106@icloud.com>
2023-09-02 18:07:32 -0400

    wef

--- profile.php
+++ profile.php
@@ -117,7 +117,7 @@
 
     <?php include "assets/includes/footer.php" ?>
 
-    <script src="assets/scripts/profile.js?v98"></script>
+    <script src="assets/scripts/profile.js?v99"></script>
 </body>
 
 </html>
