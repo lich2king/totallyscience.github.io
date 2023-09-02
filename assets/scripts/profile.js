39f3fb826879cb68d1ad8c11385b3e3aca3629c6
HackedZach <zay0106@icloud.com>
2023-09-02 18:07:26 -0400

    wef

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -98,23 +98,26 @@ document.addEventListener('DOMContentLoaded', async () => {
     let res = await fetcher(`/points/shop/unlocked`);
     let minis = await res.text();
 
+    let rarities = {
+        common: false,
+        rare: false,
+        epic: false,
+        legendary: false,
+    };
+
     minis = minis.split(';');
 
     for (let i = 1; i < minis.length; i++) {
         if (minis[i] == 'undefined') continue;
-        console.log(minis[i]);
-        document.getElementsByName(minis[i])[0].style.display = '';
-    }
 
-    // Hide the <p> elements for categories with no displayed images
-    const categories = document.querySelectorAll('.content > div > p');
-    categories.forEach((category) => {
-        const minisDiv = category.nextElementSibling; // Get the corresponding <div class="minis">
-        const images = minisDiv.querySelectorAll('img[style="display:"]'); // Find displayed images
-        if (images.length === 0) {
-            category.style.display = 'none'; // Hide the <p> element if no displayed images
+        document.getElementsByName(minis[i])[0].style.display = '';
+        let rarity = document.getElementsByName(minis[i])[0].parentElement.getAttribute('rarity');
+        console.log(rarity);
+        if (rarities.hasOwnProperty(rarity)) {
+            rarities[rarity] = true;
         }
-    });
+    }
+    console.log(rarities);
 });
 
 function createGameButton(game, pin) {
