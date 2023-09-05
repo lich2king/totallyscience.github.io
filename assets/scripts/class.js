3e456d550e71215f78823d2a053caf93db3a6d11
HackedZach <zay0106@icloud.com>
2023-09-04 21:54:03 -0400

    kolkolkp

--- assets/scripts/class.js
+++ assets/scripts/class.js
@@ -115,9 +115,7 @@ window.addEventListener('load', async () => {
         // display user like and pin status of game
         displayUserData();
 
-        let socket = io(
-            location.host.startsWith('localhost') || location.host.startsWith('127.0.0.1') ? localServer : 'https://' + location.host + '/api'
-        );
+        let socket = io(activeServer);
 
         socket.on('request-introduction', () => {
             socket.emit('respond-introduction', JSON.stringify({ name: json.username, id: json.id, mini: json.activeMini, game: gameName }));
--- class.php
+++ class.php
@@ -177,7 +177,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v171"></script>
+    <script src="assets/scripts/class.js?v172"></script>
 </body>
 
 </html>
