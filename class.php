c5b98e16039f1fab13974a8c5fcf06c7827fe512
HackedZach <zay0106@icloud.com>
2023-09-04 21:44:49 -0400

    wef

--- class.php
+++ class.php
@@ -177,7 +177,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v170"></script>
+    <script src="assets/scripts/class.js?v171"></script>
 </body>
 
 </html>
