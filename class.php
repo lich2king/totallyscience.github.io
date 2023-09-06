b5ca118aa2dadd178bf2eaf126fb26bac118b2f3
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-09-05 22:21:27 -0400

    testing another fix with nginx

--- class.php
+++ class.php
@@ -177,7 +177,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v176"></script>
+    <script src="assets/scripts/class.js?v177"></script>
 </body>
 
 </html>
