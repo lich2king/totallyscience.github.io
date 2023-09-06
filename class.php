911fa52f6e2361898eee9fcc73e6fe2bb83b3f62
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-09-05 22:13:22 -0400

    oops

--- class.php
+++ class.php
@@ -177,7 +177,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v175"></script>
+    <script src="assets/scripts/class.js?v176"></script>
 </body>
 
 </html>
