b2c3c05d58f9692f114cb29903c186022e4a3682
TotallyScience <zay0106@icloud.com>
2023-09-06 13:20:30 -0400

    offset height

--- class.php
+++ class.php
@@ -102,7 +102,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v196"></script>
+    <script src="assets/scripts/class.js?v197"></script>
 </body>
 
 </html>
