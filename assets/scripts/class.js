50de04660b9d3c6e6cebb4ba51c1ac403bb41fdb
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-09-05 22:42:09 -0400

    test

--- assets/scripts/class.js
+++ assets/scripts/class.js
@@ -115,9 +115,7 @@ window.addEventListener('load', async () => {
         // display user like and pin status of game
         displayUserData();
 
-        let socket = io(activeServer, {
-            "transports": ['websocket']
-        });
+        let socket = io(activeServer);
 
         socket.on('request-introduction', () => {
             socket.emit('respond-introduction', JSON.stringify({ name: json.username, id: json.id, mini: json.activeMini, game: gameName }));
--- class.php
+++ class.php
@@ -177,7 +177,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v178"></script>
+    <script src="assets/scripts/class.js?v179"></script>
 </body>
 
 </html>
