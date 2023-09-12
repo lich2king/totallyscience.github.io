4311b9bed6b08ab0b7acf475ebf04cea3aa259e4
TotallyScience <zay0106@icloud.com>
2023-09-12 13:14:24 -0400

    ewf

--- class.php
+++ class.php
@@ -102,7 +102,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v211"></script>
+    <script src="assets/scripts/class.js?v212"></script>
 </body>
 
 </html>
