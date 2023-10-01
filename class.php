052ba78edb0549a654d4e3003a2ca45cd571ba46
HackedZach <zay0106@icloud.com>
2023-10-01 18:36:32 -0400

    games uncache

--- class.php
+++ class.php
@@ -101,7 +101,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v221"></script>
+    <script src="assets/scripts/class.js?v222"></script>
 </body>
 
 </html>
