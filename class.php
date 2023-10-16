21f5f00453dee03c174bdbd802c77220a483c5a3
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-10-16 09:35:29 -0400

    cache

--- class.php
+++ class.php
@@ -101,7 +101,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v223"></script>
+    <script src="assets/scripts/class.js?v224"></script>
 </body>
 
 </html>
