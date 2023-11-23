89233f332e184cf76fb82d189f891da30bff0c47
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-11-23 07:52:30 -0500

    attempt fix

--- class.php
+++ class.php
@@ -96,7 +96,7 @@
     <?php include "assets/includes/footer.php" ?>
 
     <script src="assets/scripts/socket.io.min.js"></script>
-    <script src="assets/scripts/class.js?v27"></script>
+    <script src="assets/scripts/class.js?v28"></script>
 </body>
 
 </html>
