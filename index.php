fe040590eac79808bae879a0cdc7d8807f0a72b2
Ilan <53833154+IlanLuci@users.noreply.github.com>
2023-11-29 19:02:32 -0500

    move the play now button down more to leave space for multi-line feature names

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <?php include "assets/includes/head.php" ?>
 
-    <link rel='stylesheet' href='assets/styles/index.css?v48'>
+    <link rel='stylesheet' href='assets/styles/index.css?v49'>
 </head>
 
 <body>
