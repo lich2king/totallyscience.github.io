f875982ed923b2b4c535290ea32751b9e61574e0
lich2king <lich2king@yahoo.com>
2024-03-12 23:46:20 +0200

    Add files via upload

---
+++ games/2024/mjs/drift-boss/custom.js
@@ -0,0 +1,14 @@
+function _GAME_LOADED() {
+  //INJECT YOUR OWN CODE HERE
+  console.log("GAME LOADED");
+}
+
+function _START_GAME() {
+  //INJECT YOUR OWN CODE HERE
+  console.log("START GAME");
+}
+
+function _END_GAME() {
+  //INJECT YOUR OWN CODE HERE
+  console.log("END GAME");
+}
