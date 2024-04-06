7ae31f903eccb887263524ffeb06f64d1969479a
lich2king <lich2king@yahoo.com>
2024-04-07 01:47:16 +0300

    Update class.js

--- assets/scripts/class.js
+++ assets/scripts/class.js
@@ -207,7 +207,7 @@ window.addEventListener('load', async () => {
     document.querySelector('meta[name="DC.description"]').setAttribute('content', metaDesc);
     document.querySelector('meta[property="og:description"]').setAttribute('content', metaDesc);
     document.querySelector('meta[name="twitter:description"]').setAttribute('content', metaDesc);
-    document.getElementsByTagName('title')[0].innerHTML = `Totally Science - ${gameName} || Play ${gameName} unblocked on Totally Science`;
+    document.getElementsByTagName('title')[0].innerHTML = `Definitely Science - ${gameName} || Play ${gameName} unblocked on Definitely Science`;
     document.getElementsByTagName('iframe')[0].title = `${gameName} Unblocked`;
     document.getElementById('game-title').innerText = `${gameName} Unblocked`;
 
