7a691f94dc15c5f927ed79fb97b411e769a1a1fc
lich2king <lich2king@yahoo.com>
2024-04-11 16:03:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -192,21 +192,7 @@
         </div>
     </div>
 
-    <script async src="https://cdn.ad.plus/player/adplus.js"></script>
-    <script data-playerPro="current">
-    (function() {
-        var s = document.querySelector('script[data-playerPro="current"]');
-        s.removeAttribute("data-playerPro");
-        (playerPro = window.playerPro || []).push({
-            id: "z2I717k6zq5b",
-            after: s,
-            appParams: {
-                "C_NETWORK_CODE": "22753484437",
-                "C_WEBSITE": "totallyscience.co"
-            }
-        });
-    })();
-    </script>
+
 
     <div class="adsrc" style="text-align:center; margin-top:50px;">
         <!-- Horizontal Ad -->
