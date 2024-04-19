adfae788056a6aea60548eca82ba3b014e6e25a4
lich2king <lich2king@yahoo.com>
2024-04-19 16:22:00 +0300

    Update apps.php

--- apps.php
+++ apps.php
@@ -172,6 +172,29 @@
 	<a class="tn_banner" target="_blank" href="https://discord.com/invite/6kTskTSTFM">Join us on Definitely Science Discord server.</a>
 
     <div id="apps"></div>
+	
+	
+	<div id="disqus_thread" style="margin-left: 100px; margin-right: 100px;"></div>
+	<script>
+		/**
+		*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+		*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+		/*
+		var disqus_config = function () {
+		this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+		this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+		};
+		*/
+		(function() { // DON'T EDIT BELOW THIS LINE
+		var d = document, s = d.createElement('script');
+		s.src = 'https://definitely-science.disqus.com/embed.js';
+		s.setAttribute('data-timestamp', +new Date());
+		(d.head || d.body).appendChild(s);
+		})();
+	</script>
+
+	<script id="dsq-count-scr" src="//definitely-science.disqus.com/count.js" async></script>
+	
 
     <?php include "assets/includes/footer.php" ?>
 
