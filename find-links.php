ecca453bf5cc1daae88edda93b356bbfc06dd345
lich2king <lich2king@yahoo.com>
2024-05-09 10:43:23 +0300

    Create find-links.php

---
+++ find-links.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "assets/includes/head.php" ?>
+
+    <title>Find Links - Definitely Science</title>
+	<link rel="canonical" href="/" />
+
+    <link rel='stylesheet' href='./assets/styles/account.css?v1'>
+</head>
+
+<body>
+    <?php include "assets/includes/navbar.php" ?>
+
+
+    <div style="height: 20vh"></div>
+    <div style="width: 100vw; display: flex; align-items: center; justify-content: center;">
+        <h1>Looking for links</h1>
+    </div>
+    <div style="height: 35vh"></div>
+    <?php include "assets/includes/footer.php" ?>
+    
+    <script>
+        
+		
+    </script>
+</body>
+
+</html>
