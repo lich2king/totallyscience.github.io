a60de3697c9ec0dfd2ca1727520754bd8f237828
lich2king <lich2king@yahoo.com>
2024-06-11 12:04:55 +0300

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -218,7 +218,6 @@ async function loadLikedGames() {
 }
 
 async function loadPartners() {
-	console.error("load partners");
     // load partners
     let partnersRes = await fetcher(`/partners`);
     let partners = await partnersRes.json();
